17cdaf9ab84aeab27ba3c22c33f69b01a8f8de09
Carlodeboer <carlodb@hotmail.com>
2016-11-20 12:44:47 +0100

    test

--- index.php
+++ index.php
@@ -1,14 +1,13 @@
-@@ -1,3 +1,4 @@
-@@ -1,12 +1,4 @@
+
 <?php
   include "dbconnect.php";
 ?>
-@@ -10,24 +11,25 @@ and open the template in the editor.
+
 <html>
     <head>
         <title>TODO supply a title</title>
         <meta charset="UTF-8">
-@@ -14,7 +6,13 @@ and open the template in the editor.
+
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
     <body>
@@ -18,7 +17,7 @@
             <input type="submit" name="verzenden" value="Reisinfo">
         </form>
         <h1>Offroad Motorcross Portugal</h1>
-<<<<<<< HEAD
+
         <?php
           function toon($pdo){
             $stmt = $pdo->prepare("SELECT * FROM klantenbestand");
@@ -30,13 +29,11 @@
               print "<p>".$voornaam."</p><br>";
           }
         }
-@@ -29,5 +27,7 @@ and open the template in the editor.
+
 
         toon($pdo);
         ?>
-=======
->>>>>>> origin/master
+
     </body>
 </html>
 </html>
-\ No newline at end of file
