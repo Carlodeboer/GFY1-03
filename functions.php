3be00c2c48d850d2356ef1b0744230b95b130b40
Vredinburgh <ian@Ians-MacBook-Pro.local>
2016-11-20 14:57:08 +0100

    kleine fix

--- functions.php
+++ functions.php
@@ -1,4 +1,11 @@
 <?php
 // gelieve hier alle functies te plaatsen ;)
-
+function toon($pdo){
+	$stmt = $pdo->prepare("SELECT * FROM klantenbestand");
+	$stmt->execute();
+	while ($row = $stmt->fetch()){
+		$voornaam = $row["voornaam"];
+		print "<p>".$voornaam."</p><br>";
+	}
+}
 ?>
--- index.php
+++ index.php
@@ -1,17 +1,16 @@
-
+<!DOCTYPE html>
 <?php
   include "dbconnect.php";
+  include "functions.php";
 ?>
-
 <html>
     <head>
-        <title>TODO supply a title</title>
+        <title>Offroad Motorcross Portugal</title>
         <meta charset="UTF-8">
-
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
     <body>
-        <form method="POST" action="login.php"> 
+        <form method="POST" action="login.php">
             Vakantieweek: <input type="text" name="vakantieweek">
             Vakantienaam: <input type="text" name="vakantienaam">
             <input type="submit" name="verzenden" value="Reisinfo">
@@ -19,21 +18,8 @@
         <h1>Offroad Motorcross Portugal</h1>
 
         <?php
-          function toon($pdo){
-            $stmt = $pdo->prepare("SELECT * FROM klantenbestand");
-            $stmt->execute();
-            while ($row = $stmt->fetch())
-            {
-              $voornaam = $row["voornaam"];
-
-              print "<p>".$voornaam."</p><br>";
-          }
-        }
-
-
-        toon($pdo);
+            toon($pdo);
         ?>
-
     </body>
 </html>
 </html>
