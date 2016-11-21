78d80592a70ecd03db8ae67b0d6fa2a948d787e1
Vredinburgh <ian@102-146.dhcp.windesheim.nl>
2016-11-21 12:45:53 +0100

    hier spreekt men louter Nederlands

--- functions.php
+++ functions.php
@@ -2,11 +2,11 @@
 // gelieve hier alle functies te plaatsen ;)
 
 // werkt nog niet, is alleen een begin
-function create($pdo, $naam, $weeknr){
+function toevoegen($pdo, $naam, $weeknr){
 	$stmt = $pdo->prepare("INSERT INTO klantenbestand VALUES (?,?)");
 	$stmt->execute(array($naam, $weeknr));
 }
-function read($pdo){
+function opvragen($pdo){
 	$stmt = $pdo->prepare("SELECT * FROM klantenbestand");
 	$stmt->execute();
 	while ($row = $stmt->fetch()){
@@ -15,12 +15,12 @@ function read($pdo){
 	}
 }
 // werkt nog niet, is alleen een begin
-function update($pdo, $veld, $waarde, $nieuweWaarde){
+function wijzigen($pdo, $veld, $waarde, $nieuweWaarde){
 	$stmt = $pdo->prepare("UPDATE klantenbestand WHERE ? = ? SET ? = ?");
 	$stmt->execute(array($veld, $waarde, $veld, $nieuweWaarde));
 }
 // werkt nog niet, is alleen een begin
-function delete($pdo, $naam, $weeknr){
+function verwijderen($pdo, $naam, $weeknr){
 	$stmt = $pdo->prepare("DELETE FROM klantenbestand VALUES naam = ? AND weeknr = ?");
 	$stmt->execute(array($naam, $weeknr));
 }
