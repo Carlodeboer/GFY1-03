3078c34a774566c202ebd0dbd0dd23143efa8902
Sophie Roos <sophie@famroos.nu>
2016-11-22 15:45:37 +0100

    Foutje van Thijs

--- functions.php
+++ functions.php
@@ -28,21 +28,20 @@ function verwijderen($pdo, $naam, $weeknr) {
     $stmt->execute(array($naam, $weeknr));
 }
 
-<<<<<<< HEAD
-funtion banner(){
-  return(  <div id="container">
+function banner() {
+    return("<div id='container'>
         <nav>
-            <div id="banner">
+            <div id='banner'>
             </div>
             <ul>
-                <a href="index.php"><li>Home</li></a>
-                <a href="informatie.php"><li>Informatie</li></a>
-                <a href="boeken.php"><li>Boeken</li></a>
-                <a href="contact.php"><li>Contact</li></a>
-                <a href="login.php"><li>Login</li></a>
+                <a href='index.php'><li>Home</li></a>
+                <a href='informatie.php'><li>Informatie</li></a>
+                <a href='boeken.php'><li>Boeken</li></a>
+                <a href='contact.php'><li>Contact</li></a>
+                <a href='login.php'><li>Login</li></a>
             </ul>
-
         </nav>
-    </div>)
+    </div>");
+}
 
 ?>
