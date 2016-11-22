40fd05a1e5e11bbfd4eb84a55dee39f23dd398a9
thijsmarschalk <ThijsMarschalk@MacBook-Air-van-Thijs.local>
2016-11-22 16:07:52 +0100

    boven alles

--- boeken.php
+++ boeken.php
@@ -5,9 +5,18 @@
         <link type="text/css" rel="stylesheet" href="style.css">
     </head>
     <body>
-        <?php
-        banner();
-        ?>
+      <div id="container">
+          <nav>
+              <div id="banner">
+              </div>
+              <ul>
+                  <a href="index.php"><li>Home</li></a>
+                  <a href="informatie.php"><li>Informatie</li></a>
+                  <a href="boeken.php"><li>Boeken</li></a>
+                  <a href="contact.php"><li>Contact</li></a>
+                  <a href="login.php"><li>Login</li></a>
+                </nav>
+            </div>
         <table>
             <form method="GET">
                 <tr>
@@ -42,7 +51,6 @@
                 </tr>
             </form>
         </table>
-    </nav>
-</div>
+
 </body>
 </html>
