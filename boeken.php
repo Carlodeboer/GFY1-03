a8369be679c45adff64578daeb2cb8f794b5ca41
Sophie Roos <sophie@famroos.nu>
2016-11-22 15:22:59 +0100

    Pagina boeken.php gewijzigd, nog geen link naar DB

--- boeken.php
+++ boeken.php
@@ -1,3 +1,6 @@
+<?php
+include "functions.php";
+?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -5,19 +8,44 @@
         <link type="text/css" rel="stylesheet" href="style.css">
     </head>
     <body>
-        <div id="container">
-            <nav>
-                <div id="banner">
-                </div>
-                <ul>
-                    <a href="index.php"><li>Home</li></a>
-                    <a href="informatie.php"><li>Informatie</li></a>
-                    <a href="boeken.php"><li>Boeken</li></a>
-                    <a href="contact.php"><li>Contact</li></a>
-                    <a href="login.php"><li>Login</li></a>
-                </ul>
-
-            </nav>
-        </div>
-    </body>
+        <?php
+        banner();
+        // print ("Begindatum: " . $begindatum (uit database))
+        // print ("Einddatum: " . $einddatum (uit database))
+        ?>
+        <table>
+            <form method="GET">
+                <tr>
+                    <td>Begindatum:</td>
+                    <td></td>
+                </tr><tr>
+                    <td>Einddatum:</td>
+                    <td></td>
+                </tr><tr>
+                    <td>Vervoer van Luchthaven Portela (Lissabon):</td>
+                    <td><input type="radio" name="heen" value="ja" checked> Ja</td>
+                </tr><tr>
+                    <td></td>
+                    <td><input type="radio" name="heen" value="nee"> Nee</td>
+                </tr>
+                <tr><td>Vervoer naar Luchthaven Portela (Lissabon):</td>
+                    <td><input type="radio" name="terug" value="ja" checked> Ja</td>
+                </tr><tr>
+                    <td></td>
+                    <td><input type="radio" name="terug" value="nee"> Nee</td>
+                </tr><tr>
+                    <td>Locatie van overnachting:</td>
+                    <td><input type="radio" name="locatie" value="standaard"> Standaard locatie</td>
+                </tr><tr>
+                    <td></td>
+                    <td><input type="radio" name="locatie" value="anders"> Anders, namelijk: <input type="text" name="nieuweLocatie"></td>
+                </tr><tr>
+                    <td>Aantal personen:</td>
+                    <td><select name="aantalPersonen"><option value="1">1</option><option value="2">2</option><option value="3">3</option><option value="4">4</option></select></td>
+                </tr>
+            </form>
+        </table>
+    </nav>
+</div>
+</body>
 </html>
