5650da07937f64583b1498050dfe067ebe3eb4d7
Sophie Roos <sophie@famroos.nu>
2016-11-22 15:24:50 +0100

    banner()
    Functie banner() toegevoegd, ipv. dat die nu los in elk bestand staat,
    staat deze nu in functions.php. Nog niet uit elk bestand gehaald.

--- functions.php
+++ functions.php
@@ -28,4 +28,18 @@ function verwijderen($pdo, $naam, $weeknr) {
     $stmt->execute(array($naam, $weeknr));
 }
 
+function banner() {
+    print("<div id=\"container\">
+            <nav>
+                <div id=\"banner\">
+                </div>
+                <ul>
+                    <a href=\"index.php\"><li>Home</li></a>
+                    <a href=\"informatie.php\"><li>Informatie</li></a>
+                    <a href=\"boeken.php\"><li>Boeken</li></a>
+                    <a href=\"contact.php\"><li>Contact</li></a>
+                    <a href=\"login.php\"><li>Login</li></a>
+                </ul>");
+}
+
 ?>
