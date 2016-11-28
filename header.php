63d48560fb4313b0984a96dee0d9a368df29a680
Floris de Grip <Floris@MacBook-Pro-van-Floris.local>
2016-11-28 13:05:07 +0100

    menu

--- header.php
+++ header.php
@@ -8,8 +8,8 @@
 <nav>
 <ul>
     <a href="index.php"><li>Home</li></a>
-    <a href="informatie.php"><li>Informatie/Accommodatie</li></a>
-    <a href="prijzenpagina.php"><li>Prijzen/Boeken</li></a>
+    <a href="informatie.php"><li>Accommodatie</li></a>
+    <a href="prijzenpagina.php"><li>Boeken</li></a>
     <a href="contact.php"><li>Contact</li></a>
     <a href="login.php"><li>Login</li></a>
     <a href="galerij.php"><li>Galerij</li></a>
