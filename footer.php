667c35543183b5fded6af51104543814fb268c6a
Floris de Grip <Floris@MacBook-Pro-van-Floris.local>
2016-11-28 16:03:17 +0100

    vage shit in footer

--- footer.php
+++ footer.php
@@ -1,16 +1,6 @@
 <footer>
-    <h3> Contactgegevens </h3>
-<<<<<<< HEAD
+    <h3> Footer </h3>
     <a href="adminlogin.php">admin</a><br>
     <a href="imageupload.php">image uploader</a>
-=======
-    <?php
-    if(isset($_SESSION['user_session'])){
-        print "<a href=\"adminlogout.php\">logout</a>";
-    }
-    else {
-        print "<a href=\"adminlogin.php\">admin</a>";
-    }
-    ?>
->>>>>>> origin/master
+
 </footer>
