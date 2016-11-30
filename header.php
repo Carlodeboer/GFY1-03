5f63d013f43a311d4620fbfb3bf67713701bd46d
Sophie Roos <sophie@famroos.nu>
2016-11-30 13:43:07 +0100

    Prijzen aangepast

--- header.php
+++ header.php
@@ -11,7 +11,7 @@
     include je de header in een bestand in een andere map-->
     <a href=<?php print "\"http://".$_SERVER['HTTP_HOST']."/GFY1-03/index.php\"";?>><li>Home</li></a>
     <a href=<?php print "\"http://".$_SERVER['HTTP_HOST']."/GFY1-03/informatie.php\"";?>><li>Accommodatie</li></a>
-    <a href=<?php print "\"http://".$_SERVER['HTTP_HOST']."/GFY1-03/prijzenpagina.php\"";?>><li>Boeken</li></a>
+    <a href=<?php print "\"http://".$_SERVER['HTTP_HOST']."/GFY1-03/prijzen.php\"";?>><li>Prijzen</li></a>
     <a href=<?php print "\"http://".$_SERVER['HTTP_HOST']."/GFY1-03/contact.php\"";?>><li>Contact</li></a>
     <a href=<?php print "\"http://".$_SERVER['HTTP_HOST']."/GFY1-03/login.php\"";?>><li>Login</li></a>
     <a href=<?php print "\"http://".$_SERVER['HTTP_HOST']."/GFY1-03/galerij.php\"";?>><li>Galerij</li></a>
---
+++ prijzen.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Prijzen</title>
+        <?php include 'head.php'; ?>
+
+    </head>
+    <body>
+        <div id="container">
+            <?php include 'header.php'; ?>
+
+            <div id="content">
+                <h2>Prijzen</h2>
+                <p>
+                    Bibendum curabitur eu convallis facilisi venenatis arcu in a est suspendisse a a in parturient habitant curabitur dis class non quisque. Lectus fermentum suscipit nulla enim velit vestibulum blandit sem semper condimentum primis a scelerisque commodo duis cubilia vehicula sapien laoreet integer ut ante aliquet sem. Ut ut varius vestibulum vehicula mollis a felis at sagittis molestie a ullamcorper ullamcorper litora.<br> Parturient eget parturient eros semper ipsum neque parturient feugiat sed torquent consectetur id ut parturient parturient a ipsum vestibulum consequat et eleifend condimentum fames hac parturient ultrices nunc.
+                </p>
+                <form method="POST" action="boeken.php">
+                    <input type="submit" value="Boeken">
+                </form>
+            </div>
+            <?php include 'footer.php'; ?>
+        </div>
+    </body>
+</html>
