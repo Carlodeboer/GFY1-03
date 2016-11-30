40720107d51d04c5c8ca434c4613ca2f2fa420a8
moonbubble <r.deroo@solcon.nl>
2016-11-30 12:45:04 +0100

    galerij basis met oefenimages

--- Galerij.php
+++ Galerij.php
@@ -13,6 +13,29 @@ include "functions.php";
             <?php include 'header.php'; ?>
             <div id="content">
                 <h2> Galerij </h2>
+                <?php
+                $folder_path = 'oefenimages/'; //map van de afbeelding(en)
+
+                $num_files = glob($folder_path . "*.{JPG,jpg,gif,png,bmp}", GLOB_BRACE); //telt de bestanden
+
+                $folder = opendir($folder_path);
+
+                if ($num_files > 0) {
+                    while (false != ($file = readdir($folder))) { //dit leest de map door en $file wordt het eerst
+                        //volgende bestand die te vinden is
+                        $file_path = $folder_path . $file; //dit is de map waar het bestand in staat
+                        $extension = strtolower(pathinfo($file, PATHINFO_EXTENSION)); //haalt info uit de map
+                        if ($extension == 'jpg' || $extension == 'png' || $extension == 'gif' || $extension == 'bmp') {
+                            ?>
+                            <a href="<?php echo $file_path; ?>"><img src="<?php echo $file_path; ?>" class="galerij"  /></a>
+                            <?php
+                        }
+                    }
+                } else {
+                    echo "the folder was empty !";
+                }
+                closedir($folder);
+                ?>
             </div>
         </div>
         
