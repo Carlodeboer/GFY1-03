2e1464107560ff7ef3a3d0a913354a98cbe306ac
Floris de Grip <Floris@MacBook-Pro-van-Floris.local>
2016-12-05 14:19:23 +0100

    website width

--- js/responsiveslides.js
+++ js/responsiveslides.js
@@ -14,11 +14,11 @@
     // Default settings
     var settings = $.extend({
       "auto": true,             // Boolean: Animate automatically, true or false
-      "speed": 500,             // Integer: Speed of the transition, in milliseconds
-      "timeout": 3000,          // Integer: Time between slide transitions, in milliseconds
+      "speed": 600,             // Integer: Speed of the transition, in milliseconds
+      "timeout": 3800,          // Integer: Time between slide transitions, in milliseconds
       "pager": false,           // Boolean: Show pager, true or false
       "nav": false,             // Boolean: Show navigation, true or false
-      "random": false,          // Boolean: Randomize the order of the slides, true or false
+      "random": true,          // Boolean: Randomize the order of the slides, true or false
       "pause": false,           // Boolean: Pause on hover, true or false
       "pauseControls": true,    // Boolean: Pause when hovering controls, true or false
       "prevText": "Previous",   // String: Text for the "previous" button
