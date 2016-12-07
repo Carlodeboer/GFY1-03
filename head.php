3e0497554d9c1fd70d86fd0350f5495c0c7e4c0a
Vredinburgh <ian@Ians-MacBook-Pro.local>
2016-12-07 14:22:56 +0100

    hoofd

--- head.php
+++ head.php
@@ -1,7 +1,7 @@
-<link type="text/css" rel="stylesheet" href="style/style.css">
-<link type="text/css" rel="stylesheet" href="style/responsiveslides.css">
+<link type="text/css" rel="stylesheet" href=<?php print "\"http://".$_SERVER['HTTP_HOST']."/GFY1-03/style/style.css\"";?>>
+<link type="text/css" rel="stylesheet" href=<?php print "\"http://".$_SERVER['HTTP_HOST']."/GFY1-03/style/responsiveslides.css\"";?>>
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
-<script src="js/responsiveslides.js"></script>
+<script src=<?php print "\"http://".$_SERVER['HTTP_HOST']."/GFY1-03/js/responsiveslides.js\"";?>></script>
 <script src='https://www.google.com/recaptcha/api.js'></script>
 <script>
   $(function() {
