2a4416e5a28850f5e64f6f3951b04d990361cf35
thijsmarschalk <ThijsMarschalk@MacBook-Air-van-Thijs.local>
2016-12-08 19:34:59 +0100

    zdsf

--- uploadform.php
+++ uploadform.php
@@ -1,10 +1,9 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html PUBLIC >
 
   <html>
   <head><title>File Upload To Database</title></head>
   <body>
-  <h2>Please Choose a File and click Submit</h2>
+  <h2>Afbeelding Uploaden </h2>
   <form enctype="multipart/form-data" action="<?php echo htmlentities($_SERVER['PHP_SELF']);?>" method="post">
   <input type="hidden" name="MAX_FILE_SIZE" value="99999999" />
   <div><input name="userfile" type="file" /></div>
@@ -17,14 +16,14 @@
 /*** check if a file was submitted ***/
 if(!isset($_FILES['userfile']))
     {
-    echo '<p>Please select a file</p>';
+    echo '<p>Kies een Bestand</p>';
     }
 else
     {
     try    {
         upload();
         /*** give praise and thanks to the php gods ***/
-        echo '<p>Thank you for submitting</p>';
+        echo '<p>Toegevoegd</p>';
         }
     catch(Exception $e)
         {
