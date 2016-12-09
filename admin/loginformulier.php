57879dd841cf5a8560eeda8c5ed935aba55993f7
moonbubble <r.deroo@solcon.nl>
2016-12-09 10:08:03 +0100

    dit

--- admin/loginformulier.php
+++ admin/loginformulier.php
@@ -3,7 +3,7 @@
         <div class="loginmenu">
             <form method="post" action="adminlogin.php">
     		    <h2>Admin panel</h2>
-    		    Gebruikersnaam: <input type="text" name="naam"><br><br>
+    		    Gebruikersnaam: <input type="text" name="naam"><br>
     		    Wachtwoord: <input type="password" name="wachtwoord"><br><br>
     		    <input type="submit" name="submit" value="Login" class="btn-main">
     	    </form>
