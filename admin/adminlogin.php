3cb0a1710ac4c9a165e15b30950b50face8a0697
moonbubble <r.deroo@solcon.nl>
2016-12-09 10:26:40 +0100

    div adminlogin

--- admin/adminlogin.php
+++ admin/adminlogin.php
@@ -1,33 +1,37 @@
 <!DOCTYPE html>
 <html>
-<head>
-  <?php include '../head.php';?>
-</head>
+    <head>
+        <?php include '../head.php'; ?>
+    </head>
     <body>
         <div id="container">
-          <?php
-                include '../header.php';
-                //include '../functions.php';
-                if (isset($_SESSION['admin_session'])){
-                    header("Location: beheerpaneel.php");
-                } elseif (isset($_POST['naam'])) {
-                    $check = checkLogin($_POST['naam'], $_POST['wachtwoord']);
-                    if ($check['klopt']){
-                        if (checkPrivileges($_POST['naam']) >= 2) {
-                            $_SESSION['admin_session'] = $_POST['naam'];
-                            header("Location: beheerpaneel.php");
-                            exit;
+            <div id="content">
+                <div id="contentwrapper">
+                    <?php
+                    include '../header.php';
+                    //include '../functions.php';
+                    if (isset($_SESSION['admin_session'])) {
+                        header("Location: beheerpaneel.php");
+                    } elseif (isset($_POST['naam'])) {
+                        $check = checkLogin($_POST['naam'], $_POST['wachtwoord']);
+                        if ($check['klopt']) {
+                            if (checkPrivileges($_POST['naam']) >= 2) {
+                                $_SESSION['admin_session'] = $_POST['naam'];
+                                header("Location: beheerpaneel.php");
+                                exit;
+                            } else {
+
+                                print "Deze gebruikers heeft niet voldoende privileges.";
+                            }
                         } else {
-                            print "Deze gebruikers heeft niet voldoende privileges.";
+                            include 'loginformulier.php';
+                            print "<h1>" . $check['foutmelding'] . "</h1>";
                         }
                     } else {
                         include 'loginformulier.php';
-                        print "<h1>".$check['foutmelding']."</h1>";
                     }
-                } else {
-                    include 'loginformulier.php';
-                }
-            include '../footer.php';?>
+                    ?></div> </div> <?php include '../footer.php'; ?>
+
         </div>
     </body>
 </html>
