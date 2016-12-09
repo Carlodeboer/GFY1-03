4b4b8b4bd9feab331ce696eb4867b8f6ebf67c1e
moonbubble <r.deroo@solcon.nl>
2016-12-09 10:13:56 +0100

    tabel van loginformulier admin gemaakt

--- admin/loginformulier.php
+++ admin/loginformulier.php
@@ -2,11 +2,21 @@
     <div id="contentwrapper">
         <div class="loginmenu">
             <form method="post" action="adminlogin.php">
-    		    <h2>Admin panel</h2>
-    		    Gebruikersnaam: <input type="text" name="naam"><br>
-    		    Wachtwoord: <input type="password" name="wachtwoord"><br><br>
-    		    <input type="submit" name="submit" value="Login" class="btn-main">
-    	    </form>
-	   </div>
+                <h2>Admin panel</h2>
+                <table>
+                    <tr>
+                        <td>Gebruikersnaam: </td>
+                        <td><input type="text" name="naam"></td>
+                    </tr>
+                    <tr>
+                        <td>Wachtwoord: </td>
+                        <td><input type="password" name="wachtwoord"></td>
+                    </tr>
+                    <tr>
+                        <td><input type="submit" name="submit" value="Login" class="btn-main"></td>
+                    </tr>
+                </table>
+            </form>
+        </div>
     </div>
 </div>
