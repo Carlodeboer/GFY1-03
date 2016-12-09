96b648015463aa25d60452177ff0923c3838141a
Sophie Roos <sophie@famroos.nu>
2016-12-09 10:01:57 +0100

    login

--- login.php
+++ login.php
@@ -1,28 +1,36 @@
 <!DOCTYPE html>
 <html>
-    <?php
-    include "dbconnect.php"
-    ?>
-    <head>
-            <title>Motorcross</title>
-            <?php include 'head.php';?>
-    </head>
-    <body>
-        <div id="container">
+<?php
+include "dbconnect.php"
+?>
+<head>
+     <title>Motorcross</title>
+     <?php include 'head.php';?>
+</head>
+<body>
+     <div id="container">
           <?php include 'header.php';?>
-
-            <div id="content">
-              <div id="contentwrapper">
-                <h2> Login </h2>
-                <div class="loginmenu">
-                <form method="POST" action="reisinformatie.php">
-                    Vakantieweek: <input type="text" name="vakantieweek"><br><br>
-                    Vakantienaam: <input type="text" name="vakantienaam"><br><br>
-                    <input type="submit" name="verzenden" value="Reisinformatie ophalen" class="btn-main">
-                </form>
-              </div>
-              </div>
-            </div>
-            <?php include 'footer.php';?>
-        </div>
-    </body>
+          <div id="content">
+               <div id="contentwrapper">
+                    <h2>Login</h2>
+                    <div class="loginmenu">
+                         <form method="POST" action="reisinformatie.php">
+                              <table>
+                                   <tr>
+                                        <td>Vakantienaam:</td>
+                                        <td><input type="text" name="vakantieweek"></td>
+                                   </tr><tr>
+                                        <td>Vakantieweek:</td>
+                                        <td><input type="text" name="vakantienaam"></td>
+                                   </tr><tr>
+                                        <td><input type="submit" name="verzenden" value="Reisinformatie ophalen" class="btn-main"></td>
+                                   </tr>
+                              </table>
+                         </form>
+                    </div>
+               </div>
+          </div>
+          <?php include 'footer.php';?>
+     </div>
+</body>
+</html>
