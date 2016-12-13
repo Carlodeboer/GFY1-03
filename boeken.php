7b84d4ad12b4826067bec5f99f6145624512a716
Sophie Roos <sophie@famroos.nu>
2016-12-13 15:10:40 +0100

    Ziet er beter uit, nog niet klaar

--- boeken.php
+++ boeken.php
@@ -10,52 +10,77 @@
           <?php include 'header.php'; ?>
           <div id="content">
                <div id="contentwrapper">
-                    <h2> Boeken </h2>
-
+                    <h2>Boeken</h2>
                     <form method="POST" action="boekengegevens.php">
-                         <div class="form-group label-floating is-empty">
+                         <div class="form-group label-static is-empty">
                               <label for="i5i" class="control-label">Begindatum*</label>
                               <input type="date" name="begindatum" class="form-control" id="i5i" required>
                               <span class="help-block">Voer een begindatum in</span>
                          </div>
-                         <div class="form-group label-floating is-empty">
+                         <div class="form-group label-static is-empty">
                               <label for="i5i" class="control-label">Einddatum*</label>
                               <input type="date" name="einddatum" class="form-control" id="i5i" required>
                               <span class="help-block">Voer een einddatum in</span>
                          </div>
-                         <div class="form-group label-floating is-empty">
+                         <div class="form-group label-static is-empty">
                               <label for="i5i" class="control-label">Aantal personen*</label>
-                              <select name="aantalPersonen" required class="form-control" id="i5i"><option value="1">1</option><option value="2">2</option><option value="3">3</option><option value="4">4</option></select>
-                              <span class="help-block">Voer een aantal in</span>
+                              <select id="s1" class="form-control">
+                                   <option value="1">1</option>
+                                   <option value="2">2</option>
+                                   <option value="3">3</option>
+                                   <option value="4">4</option>
+                              </select>
                          </div>
-                         <div class="form-group label-floating is-empty">
+                         <div class="form-group label-static is-empty">
                               <label for="i5i" class="control-label">Vervoer van Luchthaven Portela (Lissabon)*</label>
-                              <input type="radio" name="vervoerHeen" value="1" checked class="form-control" id="i5i"> Ja
-                              <input type="radio" name="vervoerHeen" value="0" class="form-control" id="i5i"> Nee
-                              <span class="help-block">Voer een antwoord in</span>
+                              <div class="radio">
+                                   <label>
+                                        <input type="radio" name="vervoerHeen" value="1" checked> Ja
+                                   </label>
+                              </div>
+                              <div class="radio">
+                                   <label>
+                                        <input type="radio" name="vervoerHeen" value="0" checked> Nee
+                                   </label>
+                              </div>
                          </div>
-                         <div class="form-group label-floating is-empty">
+                         <div class="form-group label-static is-empty">
                               <label for="i5i" class="control-label">Vervoer naar Luchthaven Portela (Lissabon)*</label>
-                              <input type="radio" name="vervoerTerug" value="1" checked class="form-control" id="i5i"> Ja
-                              <input type="radio" name="vervoerTerug" value="0" class="form-control" id="i5i"> Nee
-                              <span class="help-block">Voer een antwoord in</span>
+                              <div class="radio">
+                                   <label>
+                                        <input type="radio" name="vervoerTerug" value="1" checked> Ja
+                                   </label>
+                              </div>
+                              <div class="radio">
+                                   <label>
+                                        <input type="radio" name="vervoerTerug" value="0"> Nee
+                                   </label>
+                              </div>
                          </div>
-                         <div class="form-group label-floating is-empty">
+                         <div class="form-group label-static is-empty">
                               <label for="i5i" class="control-label">Locatie van overnachting*</label>
-                              <input type="radio" name="locatie" value="standaard" checked class="form-control" id="i5i"> Standaard locatie
-                              <input type="radio" name="locatie" value="anders" class="form-control" id="i5i"> Anders, namelijk: <input type="text" name="nieuweLocatie" class="form-control" id="i5i">
-                              <span class="help-block">Voer een antwoord in</span>
+                              <div class="radio">
+                                   <label>
+                                        <input type="radio" name="locatie" value="standaard" checked=""> Standaard locatie
+                                   </label>
+                              </div>
+                              <div class="radio">
+                                   <label>
+                                        <input type="radio" name="vervoerTerug" value="0"> Anders, namelijk:
+                                        <input type="text" name="nieuweLocatie" class="form-control">
+                                   </label>
+                              </div>
                          </div>
-                         <div class="form-group label-floating is-empty">
+                         <div class="form-group label-static is-empty">
                               <label for="i5i" class="control-label">Opmerkingen</label>
-                              <textarea name="opmerkingen" rows="4" cols="60" class="form-control" id="i5i"></textarea>
-                              <span class="help-block">Eventuele opmerkingen</span>
+                              <textarea name="opmerkingen" class="form-control"></textarea>
                          </div>
-                         <div class="form-group label-floating is-empty">
+                         <div class="form-group label-static is-empty">
                               <label for="i5i" class="control-label">Vakantienaam*</label>
                               <input type="text" name="vakantienaam" class="form-control" id="i5i" required>
                               <span class="help-block">Deze naam gebruikt u later uw reisgegevens in te zien. Deze gegevens zou u ook eventueel kunnen delen met reisgenoten.</span>
-                              <br><br>
+                         </div>
+                         <div class="form-group label-static is-empty">
                               <input type="submit" name="volgende" value="Volgende" class="btn btn-raised btn-primary">
                          </div>
                     </form>
