945df4cf453b9196d4829d8b391b701508dc233d
Sophie Roos <sophie@famroos.nu>
2016-12-13 10:37:41 +0100

    ootstraps

--- boeken.php
+++ boeken.php
@@ -1,71 +1,67 @@
 <?php define("toegang", true); ?>
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Boeken</title>
-        <?php include 'head.php'; ?>
-    </head>
-    <body>
-        <div id="container">
-            <?php include 'header.php'; ?>
-            <div id="content">
-                <div id="contentwrapper">
+<head>
+     <title>Boeken</title>
+     <?php include 'head.php'; ?>
+</head>
+<body>
+     <div id="container">
+          <?php include 'header.php'; ?>
+          <div id="content">
+               <div id="contentwrapper">
                     <h2> Boeken </h2>
-                    <table>
-                        <form method="POST" action="boekengegevens.php">
-                            <tr>
-                                <td>Begindatum* :</td>
-                                <td><!--$begindatum--></td>
-                            </tr><tr>
-                                <td>Einddatum* :</td>
-                                <td><!--$einddatum--></td>
-                            </tr><tr>
-                                <td>Aantal personen* :</td>
-                                <td><select name="aantalPersonen" required><option value="1">1</option><option value="2">2</option><option value="3">3</option><option value="4">4</option></select></td>
-                            </tr><tr>
-                                <td>Vervoer van Luchthaven Portela (Lissabon)* :</td>
-                                <td><input type="radio" name="vervoerHeen" value="1" checked> Ja</td>
-                            </tr><tr>
-                                <td></td>
-                                <td><input type="radio" name="vervoerHeen" value="0" <?php
-                                    if (isset($_POST["vervoerHeen"]) && $_POST["vervoerHeen"] == 0) {
-                                        print ("checked");
-                                    }
-                                    ?>> Nee</td>
-                            </tr><tr>
-                                <td>Vervoer naar Luchthaven Portela (Lissabon)* :</td>
-                                <td><input type="radio" name="vervoerTerug" value="1" checked> Ja</td>
-                            </tr><tr>
-                                <td></td>
-                                <td><input type="radio" name="vervoerTerug" value="0" <?php
-                                    if (isset($_POST["vervoerTerug"]) && $_POST["vervoerTerug"] == 0) {
-                                        print ("checked");
-                                    }
-                                    ?>> Nee</td>
-                            </tr><tr>
-                                <td>Locatie van overnachting* :</td>
-                                <td><input type="radio" name="locatie" value="standaard" checked> Standaard locatie</td>
-                            </tr><tr>
-                                <td></td>
-                                <td><input type="radio" name="locatie" value="anders"> Anders, namelijk: <input type="text" name="nieuweLocatie"></td>
-                            </tr><tr>
-                                <td>Opmerkingen:</td>
-                                <td><textarea name="opmerkingen" rows="4" cols="60"></textarea></td>
-                            </tr><tr>
-                                <td>Vakantienaam* :<br>Deze naam gebruikt u later uw reisgegevens in te zien. Deze gegevens zou u ook eventueel kunnen delen met reisgenoten.</td>
-                                <td><input type="text" name="vakantienaam" required<?php
-                                    if (isset($_POST["vakantienaam"])) {
-                                        print ("value=" . $_POST["vakantienaam"]);
-                                    }
-                                    ?>></td>
-                            </tr><tr>
-                                <td><input type="submit" name="volgende" value="Volgende" class="btn btn-raised btn-primary"></td>
-                            </tr>
-                        </form>
-                    </table>
-                </div>
-            </div>
-            <?php include 'footer.php'; ?>
-        </div>
-    </body>
+
+                    <form method="POST" action="boekengegevens.php">
+                         <div class="form-group label-floating is-empty">
+                              <label for="i5i" class="control-label">Begindatum*</label>
+                              <input type="date" name="begindatum" class="form-control" id="i5i" required>
+                              <span class="help-block">Voer een begindatum in</span>
+                         </div>
+                         <div class="form-group label-floating is-empty">
+                              <label for="i5i" class="control-label">Einddatum*</label>
+                              <input type="date" name="einddatum" class="form-control" id="i5i" required>
+                              <span class="help-block">Voer een einddatum in</span>
+                         </div>
+                         <div class="form-group label-floating is-empty">
+                              <label for="i5i" class="control-label">Aantal personen*</label>
+                              <select name="aantalPersonen" required class="form-control" id="i5i"><option value="1">1</option><option value="2">2</option><option value="3">3</option><option value="4">4</option></select>
+                              <span class="help-block">Voer een aantal in</span>
+                         </div>
+                         <div class="form-group label-floating is-empty">
+                              <label for="i5i" class="control-label">Vervoer van Luchthaven Portela (Lissabon)*</label>
+                              <input type="radio" name="vervoerHeen" value="1" checked class="form-control" id="i5i"> Ja
+                              <input type="radio" name="vervoerHeen" value="0" class="form-control" id="i5i"> Nee
+                              <span class="help-block">Voer een antwoord in</span>
+                         </div>
+                         <div class="form-group label-floating is-empty">
+                              <label for="i5i" class="control-label">Vervoer naar Luchthaven Portela (Lissabon)*</label>
+                              <input type="radio" name="vervoerTerug" value="1" checked class="form-control" id="i5i"> Ja
+                              <input type="radio" name="vervoerTerug" value="0" class="form-control" id="i5i"> Nee
+                              <span class="help-block">Voer een antwoord in</span>
+                         </div>
+                         <div class="form-group label-floating is-empty">
+                              <label for="i5i" class="control-label">Locatie van overnachting*</label>
+                              <input type="radio" name="locatie" value="standaard" checked class="form-control" id="i5i"> Standaard locatie
+                              <input type="radio" name="locatie" value="anders" class="form-control" id="i5i"> Anders, namelijk: <input type="text" name="nieuweLocatie" class="form-control" id="i5i">
+                              <span class="help-block">Voer een antwoord in</span>
+                         </div>
+                         <div class="form-group label-floating is-empty">
+                              <label for="i5i" class="control-label">Opmerkingen</label>
+                              <textarea name="opmerkingen" rows="4" cols="60" class="form-control" id="i5i"></textarea>
+                              <span class="help-block">Eventuele opmerkingen</span>
+                         </div>
+                         <div class="form-group label-floating is-empty">
+                              <label for="i5i" class="control-label">Vakantienaam*</label>
+                              <input type="text" name="vakantienaam" class="form-control" id="i5i" required>
+                              <span class="help-block">Deze naam gebruikt u later uw reisgegevens in te zien. Deze gegevens zou u ook eventueel kunnen delen met reisgenoten.</span>
+                              <br><br>
+                              <input type="submit" name="volgende" value="Volgende" class="btn btn-raised btn-primary">
+                         </div>
+                    </form>
+               </div>
+          </div>
+          <?php include 'footer.php'; ?>
+     </div>
+</body>
 </html>
--- boekengegevens.php
+++ boekengegevens.php
@@ -1,19 +1,19 @@
 <?php define("toegang", true); ?>
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title>Gegevens</title>
-        <?php include 'head.php'; ?>
-    </head>
-    <body>
-        <div id="container">
-            <?php include 'header.php'; ?>
-            <div id="content">
-                <div id="contentwrapper">
+<head>
+     <meta charset="UTF-8">
+     <title>Gegevens</title>
+     <?php include "head.php"; ?>
+</head>
+<body>
+     <div id="container">
+          <?php include "header.php"; ?>
+          <div id="content">
+               <div id="contentwrapper">
                     <?php
                     if (isset($_SESSION["klantGegevens"])) {
-                        session_unset($_SESSION["klantGegevens"]);
+                         session_unset($_SESSION["klantGegevens"]);
                     }
 
                     // $begindatum = ;
@@ -25,87 +25,126 @@
                     toevoegenAanArray("locatie", "klantGegevens", "");
 
                     if ($_POST["nieuweLocatie"] != "") {
-                        $_SESSION["klantGegevens"]["locatie"] = $_POST["nieuweLocatie"];
+                         $_SESSION["klantGegevens"]["locatie"] = $_POST["nieuweLocatie"];
                     }
 
                     if ($_POST["opmerkingen"] != "") {
-                        toevoegenAanArray("opmerkingen", "klantGegevens", "");
+                         toevoegenAanArray("opmerkingen", "klantGegevens", "");
                     } else {
-                        $_SESSION["klantGegevens"]["opmerkingen"] = NULL;
+                         $_SESSION["klantGegevens"]["opmerkingen"] = NULL;
                     }
                     extract($_SESSION["klantGegevens"]);
                     ?>
 
-                    <table>
-                        <form method = "POST" action = "boekengegevenscheck.php">
-                            <tr>
-                                <td><h2>Gegevens</h2></td>
-                            </tr>
-                            <?php
-                            for ($i = 1; $i <= $aantalPersonen; $i++) {
-                                if ($aantalPersonen != 1) {
-                                    print("<tr><td><h3>Persoon " . $i . ":</h3></td></tr>");
-                                }
-                                ?>
-                                <tr><td>Voornaam* :</td><td><input type='text' name='voornaam<?php print ($i); ?>'<?php
-                                        if (isset($_POST["voornaam" . $i])) {
-                                            print (" value=" . $_POST["voornaam" . $i]);
-                                        }
-                                        ?> required></td></tr>
-                                <tr><td>Achternaam* :</td><td><input type='text' name='achternaam<?php print ($i); ?>'<?php
-                                        if (isset($_POST["achternaam" . $i])) {
-                                            print (" value=" . $_POST["achternaam" . $i]);
-                                        }
-                                        ?> required></td></tr>
-                                <tr><td>Straatnaam* :</td><td><input type='text' name='straat<?php print ($i); ?>'<?php
-                                        if (isset($_POST["straat" . $i])) {
-                                            print (" value=" . $_POST["straat" . $i]);
-                                        }
-                                        ?> required></td></tr>
-                                <tr><td>Huisnummer* :</td><td><input type='number' name='huisnummer<?php print ($i); ?>'<?php
-                                        if (isset($_POST["huisnummer" . $i])) {
-                                            print (" value=" . $_POST["huisnummer" . $i]);
-                                        }
-                                        ?> required></td></tr>
-                                <tr><td>Postcode* :</td><td><input type='text' name='postcode<?php print ($i); ?>'<?php
-                                        if (isset($_POST["postcode" . $i])) {
-                                            print (" value=" . $_POST["postcode" . $i]);
-                                        }
-                                        ?> required></td></tr>
-                                <tr><td>Woonplaats* :</td><td><input type='text' name='woonplaats<?php print ($i); ?>'<?php
-                                        if (isset($_POST["woonplaats" . $i])) {
-                                            print (" value=" . $_POST["woonplaats" . $i]);
-                                        }
-                                        ?> required></td></tr>
-                                <tr><td>Land* :</td><td><input type='text' name='land<?php print ($i); ?>'<?php
-                                        if (isset($_POST["land" . $i])) {
-                                            print (" value=" . $_POST["land" . $i]);
-                                        }
-                                        ?> required></td></tr>
-                                <tr><td>Geboortedatum* :</td><td><input type='date' name='geboortedatum<?php print ($i); ?>'<?php
-                                        if (isset($_POST["geboortedatum" . $i])) {
-                                            print (" value=" . $_POST["geboortedatum" . $i]);
-                                        }
-                                        ?> required></td></tr>
-                                <tr><td>Telefoonnummer* :</td><td><input type='text' name='telefoonnummer<?php print ($i); ?>'<?php
-                                        if (isset($_POST["telefoonnummer" . $i])) {
-                                            print (" value=" . $_POST["telefoonnummer" . $i]);
-                                        }
-                                        ?> required></td></tr>
-                                <tr><td>Emailadres* :</td><td><input type='text' name='email<?php print ($i); ?>'<?php
-                                        if (isset($_POST["email" . $i])) {
-                                            print (" value=" . $_POST["email" . $i]);
-                                        }
-                                        ?> required></td></tr>
-                                    <?php
-                                }
-                                ?>
-                            <tr><td><input type='submit' name='afronden' value='Afronden' class="btn-main"></td></tr>
-                        </form>
+                    <h2>Gegevens</h2>
+                    <form method="POST" action="boekengegevenscheck.php">
+                         <?php
+                         for ($i = 1; $i <= $aantalPersonen; $i++) {
+                              if ($aantalPersonen != 1) {
+                                   print("<h3>Persoon " . $i . ":</h3>");
+                              }
+                              ?>
+                              <div class="form-group label-floating is-empty">
+                                   <label for="i5i" class="control-label">Voornaam*</label>
+                                   <input type="text" name="voornaam<?php print ($i); ?>" class="form-control" id="i5i" required>
+                                   <span class="help-block">Voer een voornaam in</span>
+                              </div>
+                              <div class="form-group label-floating is-empty">
+                                   <label for="i5i" class="control-label">Achternaam*</label>
+                                   <input type="text" name="achternaam<?php print ($i); ?>" class="form-control" id="i5i" required>
+                                   <span class="help-block">Voer een achternaam in</span>
+                              </div>
+                              <div class="form-group label-floating is-empty">
+                                   <label for="i5i" class="control-label">Straatnaam*</label>
+                                   <input type="text" name="straatnaam<?php print ($i); ?>" class="form-control" id="i5i" required>
+                                   <span class="help-block">Voer de straatnaam in</span>
+                              </div>
+                              <div class="form-group label-floating is-empty">
+                                   <label for="i5i" class="control-label">Huisnummer*</label>
+                                   <input type="text" name="huisnummer<?php print ($i); ?>" class="form-control" id="i5i" required>
+                                   <span class="help-block">Voer het huisnummer in</span>
+                              </div>
+                              <div class="form-group label-floating is-empty">
+                                   <label for="i5i" class="control-label">*</label>
+                                   <input type="text" name="<?php print ($i); ?>" class="form-control" id="i5i" required>
+                                   <span class="help-block">Voer  in</span>
+                              </div>
+                              <div class="form-group label-floating is-empty">
+                                   <label for="i5i" class="control-label">*</label>
+                                   <input type="text" name="<?php print ($i); ?>" class="form-control" id="i5i" required>
+                                   <span class="help-block">Voer  in</span>
+                              </div>
+                              <div class="form-group label-floating is-empty">
+                                   <label for="i5i" class="control-label">*</label>
+                                   <input type="text" name="<?php print ($i); ?>" class="form-control" id="i5i" required>
+                                   <span class="help-block">Voer  in</span>
+                              </div>
+                              <div class="form-group label-floating is-empty">
+                                   <label for="i5i" class="control-label">*</label>
+                                   <input type="text" name="<?php print ($i); ?>" class="form-control" id="i5i" required>
+                                   <span class="help-block">Voer  in</span>
+                              </div>
+                              <table>
+
+                                   <tr><td>Voornaam* :</td><td><input type="text" name="voornaam<?php print ($i); ?>"<?php
+                                   if (isset($_POST["voornaam" . $i])) {
+                                        print (" value=" . $_POST["voornaam" . $i]);
+                                   }
+                                   ?> required></td></tr>
+                                   <tr><td>Achternaam* :</td><td><input type="text" name="achternaam<?php print ($i); ?>"<?php
+                                   if (isset($_POST["achternaam" . $i])) {
+                                        print (" value=" . $_POST["achternaam" . $i]);
+                                   }
+                                   ?> required></td></tr>
+                                   <tr><td>Straatnaam* :</td><td><input type="text" name="straat<?php print ($i); ?>"<?php
+                                   if (isset($_POST["straat" . $i])) {
+                                        print (" value=" . $_POST["straat" . $i]);
+                                   }
+                                   ?> required></td></tr>
+                                   <tr><td>Huisnummer* :</td><td><input type="number" name="huisnummer<?php print ($i); ?>"<?php
+                                   if (isset($_POST["huisnummer" . $i])) {
+                                        print (" value=" . $_POST["huisnummer" . $i]);
+                                   }
+                                   ?> required></td></tr>
+                                   <tr><td>Postcode* :</td><td><input type="text" name="postcode<?php print ($i); ?>"<?php
+                                   if (isset($_POST["postcode" . $i])) {
+                                        print (" value=" . $_POST["postcode" . $i]);
+                                   }
+                                   ?> required></td></tr>
+                                   <tr><td>Woonplaats* :</td><td><input type="text" name="woonplaats<?php print ($i); ?>"<?php
+                                   if (isset($_POST["woonplaats" . $i])) {
+                                        print (" value=" . $_POST["woonplaats" . $i]);
+                                   }
+                                   ?> required></td></tr>
+                                   <tr><td>Land* :</td><td><input type="text" name="land<?php print ($i); ?>"<?php
+                                   if (isset($_POST["land" . $i])) {
+                                        print (" value=" . $_POST["land" . $i]);
+                                   }
+                                   ?> required></td></tr>
+                                   <tr><td>Geboortedatum* :</td><td><input type="date" name="geboortedatum<?php print ($i); ?>"<?php
+                                   if (isset($_POST["geboortedatum" . $i])) {
+                                        print (" value=" . $_POST["geboortedatum" . $i]);
+                                   }
+                                   ?> required></td></tr>
+                                   <tr><td>Telefoonnummer* :</td><td><input type="text" name="telefoonnummer<?php print ($i); ?>"<?php
+                                   if (isset($_POST["telefoonnummer" . $i])) {
+                                        print (" value=" . $_POST["telefoonnummer" . $i]);
+                                   }
+                                   ?> required></td></tr>
+                                   <tr><td>Emailadres* :</td><td><input type="text" name="email<?php print ($i); ?>"<?php
+                                   if (isset($_POST["email" . $i])) {
+                                        print (" value=" . $_POST["email" . $i]);
+                                   }
+                                   ?> required></td></tr>
+                                   <?php
+                              }
+                              ?>
+                              <tr><td><input type="submit" name="afronden" value="Afronden" class="btn-main"></td></tr>
+                         </form>
                     </table>
-                </div>
-            </div>
-            <?php include 'footer.php'; ?>
-        </div>
-    </body>
+               </div>
+          </div>
+          <?php include "footer.php"; ?>
+     </div>
+</body>
 </html>
