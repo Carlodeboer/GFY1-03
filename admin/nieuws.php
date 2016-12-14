3ad12d88c61a8d53ae95f1926d8bb7bf797a9ad0
Carlodeboer <carlodb@hotmail.com>
2016-12-14 13:56:55 +0100

    uitlijnen

--- admin/nieuws.php
+++ admin/nieuws.php
@@ -1,114 +1,104 @@
 <!DOCTYPE html>
 <html>
-<head>
-           <!-- core CSS of SnackbarJS, find it in /dist -->
-    <!-- <link href=dist/snackbar.min.css rel=stylesheet> -->
-    <!-- the default theme of SnackbarJS, find it in /themes-css -->
-<!--     <link href=dist/material.css rel=stylesheet> -->
-</head>
-    <body>
-    <!-- <script src=dist/snackbar.min.js></script> -->
-
-<br><br>
-<div id="contentwrapper">
-<div class="container">
-<div class="row">
-<div class="col-md-8">
-<h3>Nieuw nieuwsbericht toevoegen</h3><br>
-    <form method="post" action="beheerpaneel.php?beheer=Nieuws">
-    <div class="form-group">
-      <label for="inputtitel" class="col-md-2 control-label">Titel</label>
-
-      <div class="col-md-10">
-        <input type="text" name="titel" class="form-control" id="inputtitel" placeholder="Titel">
-      </div>
-      </div><br><br>
-
-  <div class="form-group">
-      <label for="select111" class="col-md-2 control-label">Taal</label>
-
-      <div class="col-md-10">
-        <select name="lang" id="select111" class="form-control">
-            <option value="NLD">Nederlands</option>
-            <option value="ENG">Engels</option>
-            <option value="DEU">Duits</option>
-        </select>
-      </div>
-    </div> <br><br>
-
-​    <div class="form-group is-empty">
-      <label for="textArea" class="col-md-2 control-label">Bericht</label>
-
-      <div class="col-md-10">
-        <textarea class="form-control" rows="5" name="bodytext" id="textArea"></textarea>
-        <span class="help-block">Vul hier een bericht in</span>
-      </div>
-    </div><br><br><br><br>
-    <br><br><br><br>
-
-
-<input type="submit" name="plaatsnieuws" value="Plaatsen" class="btn btn-raised btn-primary">
-<input type="reset" value="Annuleren" class="btn btn-raised btn-primary">
-</form>
-
-<br><br>
-
-<form method="post" action="beheerpaneel.php?beheer=Nieuwsbewerken">
-<input type="submit" name="nieuwsbewerken" value="Artikelen bewerken" class="btn btn-raised btn-primary">
-</form>
-
-
-<script>
-function popup() {
- $("#123").snackbar("show");
- }
-
-</script>
-
-
-
-<?php
-        include "../dbconnect.php";
-        // include "../functions.php";
-
-        date_default_timezone_set('Europe/Amsterdam');
-$date = date('d F Y H:i', time());
-
-
-if (isset($_POST['plaatsnieuws'])) {
-    $titel = $_POST['titel'];
-    $lang = $_POST['lang'];
-    $bodytext = $_POST['bodytext'];
-
-?>
-<span data-toggle=snackbar id="123" data-content="Het artikel <?php print($titel); ?> is toegevoegd."></span>
-<?php
-
-    if ($titel == "") {
-        print("Voer een titel in.");
-    } elseif ($bodytext == "") {
-        print("Voer een bericht in.");
-    } else {
-
-        $stmt = $pdo->prepare("INSERT INTO nieuwsbericht (lang,title,bodytext,posted) VALUES (?,?,?,?)");
-        $stmt->execute(array($lang, $titel, $bodytext,$date));
-        $userRow = $stmt->fetch(PDO::FETCH_ASSOC);
-        $res = $stmt->rowCount();
-        if ($res > 0) {
-            //feedback aan gebruiker geven
-            print("Het bericht " . $titel . " is toegevoegd.");
-            print("<script>window.onload = popup;</script>");
-            // $userRow=$stmt->fetch(PDO::FETCH_ASSOC);
+    <head>
+    </head>
+    <!-- snackbar functie -->
+    <script>
+        function popup() {
+            $("#123").snackbar("show");
         }
-    }
-}
-?>
+
+    </script>
+    <body>
+
+        <br><br>
+        <div id="contentwrapper">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8">
+                        <h3>Nieuw nieuwsbericht toevoegen</h3><br>
+                        <form method="post" action="beheerpaneel.php?beheer=Nieuws">
+                            <div class="form-group">
+                                <label for="inputtitel" class="col-md-2 control-label">Titel</label>
+
+                                <div class="col-md-10">
+                                    <input type="text" name="titel" class="form-control" id="inputtitel" placeholder="Titel">
+                                </div>
+                            </div><br><br>
+
+                            <div class="form-group">
+                                <label for="select111" class="col-md-2 control-label">Taal</label>
+
+                                <div class="col-md-10">
+                                    <select name="lang" id="select111" class="form-control">
+                                        <option value="NLD">Nederlands</option>
+                                        <option value="ENG">Engels</option>
+                                        <option value="DEU">Duits</option>
+                                    </select>
+                                </div>
+                            </div> <br><br>
+
+                            ​    <div class="form-group is-empty">
+                                <label for="textArea" class="col-md-2 control-label">Bericht</label>
+
+                                <div class="col-md-10">
+                                    <textarea class="form-control" rows="5" name="bodytext" id="textArea"></textarea>
+                                    <span class="help-block">Vul hier een bericht in</span>
+                                </div>
+                            </div><br><br><br><br>
+                            <br><br><br><br>
 
 
-</div>
-</div>
-</div>
-</div>
-</div>
-    </body>
+                            <input type="submit" name="plaatsnieuws" value="Plaatsen" class="btn btn-raised btn-primary">
+                            <input type="reset" value="Annuleren" class="btn btn-raised btn-primary">
+                        </form>
+
+                        <br><br>
+
+                        <form method="post" action="beheerpaneel.php?beheer=Nieuwsbewerken">
+                            <input type="submit" name="nieuwsbewerken" value="Artikelen bewerken" class="btn btn-raised btn-primary">
+                        </form>
+
+
+                        <?php
+                        include "../dbconnect.php";
+                        
+                        date_default_timezone_set('Europe/Amsterdam');
+                        $date = date('d F Y H:i', time());
+
+
+                        if (isset($_POST['plaatsnieuws'])) {
+                            $titel = $_POST['titel'];
+                            $lang = $_POST['lang'];
+                            $bodytext = $_POST['bodytext'];
+                            ?>
+                            <span data-toggle=snackbar id="123" data-content="Het artikel <?php print($titel); ?> is toegevoegd."></span>
+                            <?php
+                            if ($titel == "") {
+                                print("Voer een titel in.");
+                            } elseif ($bodytext == "") {
+                                print("Voer een bericht in.");
+                            } else {
+// Gegevens toevoegen aan database
+                                $stmt = $pdo->prepare("INSERT INTO nieuwsbericht (lang,title,bodytext,posted) VALUES (?,?,?,?)");
+                                $stmt->execute(array($lang, $titel, $bodytext, $date));
+                                $userRow = $stmt->fetch(PDO::FETCH_ASSOC);
+                                $res = $stmt->rowCount();
+                                if ($res > 0) {
+                                    //feedback aan gebruiker geven
+                                    print("Het bericht " . $titel . " is toegevoegd.");
+                                    print("<script>window.onload = popup;</script>");
+                                    // $userRow=$stmt->fetch(PDO::FETCH_ASSOC);
+                                }
+                            }
+                        }
+                        ?>
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
 </html>
--- admin/nieuwsbewerken.php
+++ admin/nieuwsbewerken.php
@@ -1,182 +1,172 @@
 <!DOCTYPE html>
 <html>
-<head>
+    <head>
+        <script>
+            function popupverwijder() {
+                $("#verwijder").snackbar("show");
+            }
+        </script>
 
-<script>
-function popupverwijder() { 
- $("#verwijder").snackbar("show");
- }
-</script>
-
-<script>
-function popupupdate() { 
- $("#update").snackbar("show");
- }
-</script>
-
-
-
-</head>
+        <script>
+            function popupupdate() {
+                $("#update").snackbar("show");
+            }
+        </script>
+    </head>
     <body>
 
-
-<br><br>
-<h3>Nieuwsberichten bewerken</h3><br>
-<?php
-
-    include"../dbconnect.php";
-    $stmt = $pdo->prepare("SELECT id,lang,title,description,bodytext,posted
+        <br><br>
+        <h3>Nieuwsberichten bewerken</h3><br>
+        <?php
+        include"../dbconnect.php";
+        $stmt = $pdo->prepare("SELECT id,lang,title,description,bodytext,posted
                             FROM nieuwsbericht");
-    $stmt->execute();
-?>
-<br><br><br>
+        $stmt->execute();
+        ?>
+        <br><br><br>
 
 
-<!-- <div class="container"> -->
-<div class="row">
-<div class="col-md-6">
+        <!-- <div class="container"> -->
+        <div class="row">
+            <div class="col-md-6">
 
-<table class="table table-striped table-hover">
-<th>ID</th><th>Taal</th><th>Titel</th><th>Omschrijving</th><th>Datum</th>
+                <table class="table table-striped table-hover">
+                    <th>ID</th><th>Taal</th><th>Titel</th><th>Omschrijving</th><th>Datum</th>
 
-<?php
-    while($content = $stmt->fetch()) {
-echo "<tr onclick=\"location='beheerpaneel.php?beheer=Nieuwsbewerken&berichtId={$content['id']}'\">
+                    <?php
+                    while ($content = $stmt->fetch()) {
+                        echo "<tr onclick=\"location='beheerpaneel.php?beheer=Nieuwsbewerken&berichtId={$content['id']}'\">
 
 <td>" . $content['id'] . "</td>
 <td>" . $content['lang'] . "</td>
 <td>" . $content['title'] . "</td>
-<td>" . $content['description'] ."</td>
-<td>"  . $content['posted'] . "</td>
+<td>" . $content['description'] . "</td>
+<td>" . $content['posted'] . "</td>
 
 </tr>";
-}
-?>
+                    }
+                    ?>
 
-</table>
+                </table>
 
-<br>
-<form method="post" action="beheerpaneel.php?beheer=Nieuws">
-<input type="submit" value="Terug" name="terug" class="btn btn-raised btn-primary">
-</form>
-</div>
-
-
-
-<div class="col-md-6">
+                <br>
+                <form method="post" action="beheerpaneel.php?beheer=Nieuws">
+                    <input type="submit" value="Terug" name="terug" class="btn btn-raised btn-primary">
+                </form>
+            </div>
 
 
-<?php
 
-if(isset($_GET['berichtId'])) {
-    $berichtId = $_GET['berichtId'];
+            <div class="col-md-6">
 
-        $stmt = $pdo->prepare("SELECT id,lang,title,description,bodytext,posted
+
+                <?php
+                if (isset($_GET['berichtId'])) {
+                    $berichtId = $_GET['berichtId'];
+
+                    $stmt = $pdo->prepare("SELECT id,lang,title,description,bodytext,posted
                             FROM nieuwsbericht
                             WHERE id=?");
-            $stmt->execute(array($berichtId));
-            $content=$stmt->fetch();
+                    $stmt->execute(array($berichtId));
+                    $content = $stmt->fetch();
+                }
 
-}
+                $taal = 0;
 
-$taal = 0;
+                switch ($content['lang']) {
+                    case "NLD":
+                        $taal = "Nederlands";
+                        break;
+                    case "ENG":
+                        $taal = "Engels";
+                        break;
+                    case "DEU":
+                        $taal = "Duits";
+                        break;
+                }
+                ?>
 
-switch($content['lang']) {
-    case "NLD":
-    $taal = "Nederlands";
-    break;
-    case "ENG":
-    $taal = "Engels";
-    break;
-    case "DEU":
-    $taal = "Duits";
-    break;
-} 
-
-?>
-
-    <form method="post">
+                <form method="post">
 
 
-    <div class="form-group">
-      <label for="inputtitel" class="col-md-6 control-label">Titel</label>
+                    <div class="form-group">
+                        <label for="inputtitel" class="col-md-6 control-label">Titel</label>
 
-      <div class="col-md-10">
-        <input type="text" class="form-control" id="inputtitel" name="titel" placeholder="Titel" value="<?php print($content['title']);?>">
-</div>
-</div>
-            <div class="form-group">
-      <label for="inputomschrijving" class="col-md-6 control-label">Omschrijving</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" id="inputtitel" name="titel" placeholder="Titel" value="<?php print($content['title']); ?>">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="inputomschrijving" class="col-md-6 control-label">Omschrijving</label>
 
-      <div class="col-md-10">
-        <input type="text" class="form-control" id="inputomschrijving" name="omschrijving" placeholder="Omschrijving" value="<?php print($content['description']);?>">
-</div>
-</div>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" id="inputomschrijving" name="omschrijving" placeholder="Omschrijving" value="<?php print($content['description']); ?>">
+                        </div>
+                    </div>
 
-            <div class="form-group">
-      <label for="inputdatum" class="col-md-6 control-label">Datum</label>
+                    <div class="form-group">
+                        <label for="inputdatum" class="col-md-6 control-label">Datum</label>
 
-      <div class="col-md-10">
-        <input type="text" class="form-control" id="inputdatum" name="datum" placeholder="Datum" value="<?php print($content['posted']);?>">
-</div>
-</div>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" id="inputdatum" name="datum" placeholder="Datum" value="<?php print($content['posted']); ?>">
+                        </div>
+                    </div>
 
 
-  <div class="form-group">
-      <label for="select111" class="col-md-6 control-label">Taal</label>
+                    <div class="form-group">
+                        <label for="select111" class="col-md-6 control-label">Taal</label>
 
-      <div class="col-md-10">
-        <select name="lang" id="select111" class="form-control">
-            <option selected="<?php print($content['lang'])?>" value="<?php print($content['lang'])?>"><?php print($taal);?></option>
-            <option value="NLD">Nederlands</option>
-            <option value="ENG">Engels</option>
-            <option value="DEU">Duits</option>
-        </select>
-      </div>
+                        <div class="col-md-10">
+                            <select name="lang" id="select111" class="form-control">
+                                <option selected="<?php print($content['lang']) ?>" value="<?php print($content['lang']) ?>"><?php print($taal); ?></option>
+                                <option value="NLD">Nederlands</option>
+                                <option value="ENG">Engels</option>
+                                <option value="DEU">Duits</option>
+                            </select>
+                        </div>
+                    </div>
+
+                    <div class="form-group is-empty">
+                        <label for="textArea" class="col-md-6 control-label">Bericht</label>
+
+                        <div class="col-md-10">
+                            <textarea class="form-control" rows="5" name="bodytext" id="textArea"><?php print($content['bodytext']); ?></textarea>
+                            <span class="help-block">Vul hier een bericht in</span>
+                        </div>
+                    </div>
+                    <br>
+                    <br><br>
+                    <br><br>
+                    <br><br>
+                    <br><br>
+                    <br><br>
+                    <br><br>
+                    <br><br>
+                    <br>
+
+                    <input type="submit" name="updaten" value="Updaten" class="btn btn-raised btn-primary">
+                    <input class="btn btn-raised btn-warning" type="submit" name="delete" value="Verwijderen" onclick="return confirm('Weet je het zeker?')" />
+
+                </form>
+
+                <br><br>
+
+            </div>
+        </div>
     </div>
 
-        <div class="form-group is-empty">
-      <label for="textArea" class="col-md-6 control-label">Bericht</label>
-
-      <div class="col-md-10">
-        <textarea class="form-control" rows="5" name="bodytext" id="textArea"><?php print($content['bodytext']);?></textarea>
-        <span class="help-block">Vul hier een bericht in</span>
-      </div>
-    </div>
- <br>
-<br><br>
-<br><br>
-<br><br>
-<br><br>
-<br><br>
-<br><br>
-<br><br>
-<br>
-
-<input type="submit" name="updaten" value="Updaten" class="btn btn-raised btn-primary">
-<input class="btn btn-raised btn-warning" type="submit" name="delete" value="Verwijderen" onclick="return confirm('Weet je het zeker?')" />
-
-</form>
-
-<br><br>
-
-</div>
-</div>
-</div>
-
-<span data-toggle=snackbar id="verwijder" data-content="Het bericht is verwijderd."></span>
-<span data-toggle=snackbar id="update" data-content="Het bericht is bijgewerkt."></span>
+    <span data-toggle=snackbar id="verwijder" data-content="Het bericht is verwijderd."></span>
+    <span data-toggle=snackbar id="update" data-content="Het bericht is bijgewerkt."></span>
 
 <?php
-
 if (isset($_POST['updaten'])) {
     $titel = $_POST['titel'];
     $lang = $_POST['lang'];
     $omschrijving = $_POST['omschrijving'];
     $posted = $_POST['datum'];
     $bodytext = $_POST['bodytext'];
-        $berichtId = $_GET['berichtId'];
- 
+    $berichtId = $_GET['berichtId'];
+
     if ($titel == "") {
         print("Voer een titel in.");
     } elseif ($bodytext == "") {
@@ -184,62 +174,50 @@ if (isset($_POST['updaten'])) {
     } else {
 
 
-    $pdo = newPDO();
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $pdo = newPDO();
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $stmt = $pdo->prepare("UPDATE nieuwsbericht SET lang=?, title=?, description=?, bodytext=?, posted=? WHERE id=?");
-                    try
-    { 
-        $stmt->execute(array($lang, $titel, $omschrijving, $bodytext, $posted, $berichtId));
-    }
-        catch (PDOException $e)
-    {
-        echo "Er is iets fout gegaan";
-        throw $e;
-    }
+        try {
+            $stmt->execute(array($lang, $titel, $omschrijving, $bodytext, $posted, $berichtId));
+        } catch (PDOException $e) {
+            echo "Er is iets fout gegaan";
+            throw $e;
+        }
 
-$res = $stmt->rowCount();
+        $res = $stmt->rowCount();
         if ($res > 0) {
             //feedback aan gebruiker geven
             print("Het bericht " . $titel . " is bijgewerkt.");
             print("<script>window.onload = popupupdate;</script>");
         }
-        }
-    
+    }
 }
 
 
 
-if(isset($_POST['delete'])) {
-	if(!isset($_GET['berichtId'])) {
-		print("Er is geen bericht geselecteerd.");
-	} else {
+if (isset($_POST['delete'])) {
+    if (!isset($_GET['berichtId'])) {
+        print("Er is geen bericht geselecteerd.");
+    } else {
 
+        $stmt = $pdo->prepare("DELETE FROM nieuwsbericht WHERE id=?");
+        try {
+            $stmt->execute(array($berichtId));
+        } catch (PDOException $e) {
+            echo "Er is iets fout gegaan";
+            throw $e;
+        }
 
-
-
-
-      $stmt = $pdo->prepare("DELETE FROM nieuwsbericht WHERE id=?");
-                    try
-    { 
-        $stmt->execute(array($berichtId));
-    }
-        catch (PDOException $e)
-    {
-        echo "Er is iets fout gegaan";
-        throw $e;
-    }
-
-$res = $stmt->rowCount();
+        $res = $stmt->rowCount();
         if ($res > 0) {
             //feedback aan gebruiker geven
             print("Het bericht is verwijderd.");
             print("<script>window.onload = popupverwijder;</script>");
         }
+    }
 }
-}
-
 ?>
 
 
-    </body>
+</body>
 </html>
