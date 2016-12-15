0fae4196786886ec680d699e59001020ff81e162
Carlodeboer <carlodb@hotmail.com>
2016-12-15 09:53:40 +0100

    DU bijna klaar

--- ContactDU.php
+++ ContactDU.php
@@ -1,68 +1,58 @@
+<h2>Kontakt</h2>
 
-              <h2> Kontakt </h2>
             <form name="contactform" method="post" action="contactscript.php">
-            <table width="450px">
 
-            <tr>
-             <td valign="top">
-              <label for="first_name">Voornaam *</label>
-             </td>
-             <td valign="top">
-              <input  type="text" name="first_name" maxlength="50" size="30">
-             </td>
-            </tr>
 
-            <tr>
-             <td valign="top">
-              <label for="last_name">Achternaam *</label>
-             </td>
-             <td valign="top">
-              <input  type="text" name="last_name" maxlength="50" size="30">
-            </td>
-          </tr>
 
-          <tr>
-            <td valign="top">
-             <label for="subject">Onderwerp *</label>
-            </td>
-            <td valign="top">
-             <input  type="text" name="subject" maxlength="50" size="30">
-             </td>
-            </tr>
+                            <div class="form-group">
+                                <label for="inputvoornaam" class="col-md-2 control-label">Voornaam</label>
+                                <div class="col-md-10">
+                                    <input type="text" name="first_name" class="form-control" id="inputvoornaam" placeholder="Voornaam">
+                                </div>
+                                </div>
 
-           <tr>
-             <td valign="top">
-              <label for="email">Email Adres *</label>
-             </td>
-             <td valign="top">
-              <input  type="text" name="email" maxlength="80" size="30">
-             </td>
-            </tr>
+                            <div class="form-group">
+                                <label for="inputachternaam" class="col-md-2 control-label">Achternaam</label>
+                                <div class="col-md-10">
+                                    <input type="text" name="last_name" class="form-control" id="inputachternaam" placeholder="Achternaam">
+                                </div>
+                                </div>
 
-            <tr>
-             <td valign="top">
-              <label for="telephone">Telefoonnummer</label>
-             </td>
-             <td valign="top">
-              <input  type="text" name="telephone" maxlength="30" size="30">
-             </td>
-            </tr>
+                            <div class="form-group">
+                                <label for="inputonderwerp" class="col-md-2 control-label">Onderwerp</label>
+                                <div class="col-md-10">
+                                    <input type="text" name="subject" class="form-control" id="inputachternaam" placeholder="Onderwerp">
+                                </div>
+                                </div>
 
-            <tr>
-             <td valign="top">
-              <label for="comments">Bericht *</label>
-             </td>
-             <td valign="top">
-              <textarea  name="comments" maxlength="2000" cols="25" rows="6"></textarea>
-             </td>
-            </tr>
+                            <div class="form-group">
+                                <label for="inputemail" class="col-md-2 control-label">Email</label>
+                                <div class="col-md-10">
+                                    <input type="email" name="email" class="form-control" id="inputemail" placeholder="Email">
+                                </div>
+                                </div>
 
-            <tr>
-             <td colspan="2" style="text-align:center">
-              <input type="submit" value="Verzenden" class="btn-main">
-             </td>
-            </tr>
-            </table>
-            <div class="g-recaptcha" data-sitekey="6LdvmA0UAAAAANG5pBHPYoqw1-MTYUJRngrA88zK"></div>
-            </form>
+                            <div class="form-group">
+                                <label for="inputtelefoon" class="col-md-2 control-label">Telefoon</label>
+                                <div class="col-md-10">
+                                    <input type="text" name="telephone" class="form-control" id="inputtelefoon" placeholder="Telefoonnummer">
+                                </div>
+                                </div>
+
+
+    <div class="form-group">
+      <label for="textArea" class="col-md-2 control-label">Bericht</label>
+
+      <div class="col-md-10">
+        <textarea class="form-control" name="comments" rows="3" id="textArea"></textarea>
+        <span class="help-block">Max 2000 characters.</span>
+      </div>
+    </div>
+                                              
+                                                          <div class="g-recaptcha" data-sitekey="6LdvmA0UAAAAANG5pBHPYoqw1-MTYUJRngrA88zK"></div><br><br><br><br><br><br><br>
+                                                          <input type="submit" value="Verzenden" class="btn btn-raised btn-primary">
+
+</form>
+
+
       
--- contact.php
+++ contact.php
@@ -10,7 +10,8 @@
           <?php include 'header.php';?>
           <div id="content">
             <div id="contentwrapper">
-
+<div class="row">
+<div class="col-md-6">
             <?php
             if (isset($_GET['lang'])){
                 $_SESSION['lang'] = $_GET['lang'];
@@ -30,9 +31,22 @@
             elseif($taal == "ENG"){
               include 'ContactEN.php';
             }
-
-
                 ?>
+                </div>
+
+                <div class="col-md-2">
+                </div>
+
+
+<div class="col-md-4">
+    <div id="googlemaps">
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2428.853044415557!2d6.079371215736348!3d52.49989967981054!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c7ded4f4f95b31%3A0xb1a2f2cf9bba075!2sCampus+1%2C+8017+Zwolle!5e0!3m2!1snl!2snl!4v1481550637316"
+      width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+    </div>
+    </div>
+  </div>
+
+                </div>
               </div>
           </div>
           <?php include 'footer.php';?>
