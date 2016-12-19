d3b4d5919905e656808f2c63bb79ceb6fc0f9c66
moonbubble <r.deroo@solcon.nl>
2016-12-19 13:39:20 +0100

    more more more stuff

--- admin/boekopvraag.php
+++ admin/boekopvraag.php
@@ -1,25 +1,32 @@
+<html>
+<head>
+  <title>Boekingopvraag</title>
+</head>
+<body>
   <div id="contentwrapper">
-              Laat boekingen zien: <br>
-              <?php
+    Laat boekingen zien: <br>
+    <?php
 
-              $pdo = newPDO();
-              $stmt = $pdo->prepare("SELECT idKlant, begindatum FROM boeking");
-              $stmt->execute();
-              $teller = 0;
-              $i = 0;
-              $resultaat = array();
-              while($userRow = $stmt-> fetch()){
-                $resultaat[$i] = array($userRow['idKlant'], $userRow['begindatum']);
-                $i++;
-              }
-              foreach($resultaat as $oefen){
+    $pdo = newPDO();
+    $stmt = $pdo->prepare("SELECT idKlant, begindatum FROM boeking");
+    $stmt->execute();
+    $teller = 0;
+    $i = 0;
+    $resultaat = array();
+    while($userRow = $stmt-> fetch()){
+      $resultaat[$i] = array($userRow['idKlant'], $userRow['begindatum']);
+      $i++;
+    }
+    foreach($resultaat as $oefen){
 
 
-              print(" <a href=\"http://localhost/GFY1-03/admin/boekingopvraagsript.php\">" . $resultaat[$teller][0] . ",  " . $resultaat[$teller][1] . "</a>");
-              print("</br>");
-              $teller++;
+      print(" <a href=\"http://localhost/GFY1-03/admin/boekopvraagsript.php\">" . $resultaat[$teller][0] . ",  " . $resultaat[$teller][1] . "</a>");
+      print("</br>");
+      $teller++;
 
-}
-                ?>
-
-              </div>
+    }
+    ?>
+    
+  </div>
+</body>
+</html>
