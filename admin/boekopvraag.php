1f8e44d1b649220efc3d711e39023488f7a3e1d0
moonbubble <r.deroo@solcon.nl>
2016-12-19 14:07:19 +0100

    more more more more more stuff

--- admin/boekopvraag.php
+++ admin/boekopvraag.php
@@ -8,18 +8,19 @@
     <?php
 
     $pdo = newPDO();
-    $stmt = $pdo->prepare("SELECT begindatum, einddatum FROM boeking");
+    $stmt = $pdo->prepare("SELECT vakantienaam, begindatum, einddatum FROM boeking b JOIN reis r ON b.idKlant=r.idklant");
     $stmt->execute();
     $teller = 0;
     $i = 0;
     $resultaat = array();
     while($userRow = $stmt-> fetch()){
-      $resultaat[$i] = array($userRow['begindatum'], $userRow['einddatum']);
+      $resultaat[$i] = array($userRow['vakantienaam'], $userRow['begindatum'], $userRow['einddatum']);
       $i++;
     }
 
     foreach($resultaat as $oefen){
-      print(" <a href=\"http://localhost/GFY1-03/admin/boekopvraagsript.php\">" . $resultaat[$teller][0] . ",  " . $resultaat[$teller][1] . "</a>");
+      print(" <a href=\"http://localhost/GFY1-03/admin/boekopvraagsript.php\">" . $resultaat[$teller][0] . ",  "
+      . $resultaat[$teller][1] . ",  " . $resultaat[$teller][2] . "</a>");
       print("</br>");
       $teller++;
     }
