432544b5b06c7f1e92a1edd5ec7228b29afc6bb7
moonbubble <r.deroo@solcon.nl>
2016-12-20 15:23:00 +0100

    fien stuff

--- admin/boekopvraag.php
+++ admin/boekopvraag.php
@@ -1,31 +1,49 @@
 <html>
 <head>
   <title>Boekingopvraag</title>
+
 </head>
 <body>
-  <div id="contentwrapper">
-    Laat boekingen zien: <br>
-    <?php
+  <div id="container">
 
-    $pdo = newPDO();
-    $stmt = $pdo->prepare("SELECT vakantienaam, begindatum, einddatum FROM boeking b JOIN reis r ON b.idKlant=r.idklant");
-    $stmt->execute();
-    $teller = 0;
-    $i = 0;
-    $resultaat = array();
-    while($userRow = $stmt-> fetch()){
-      $resultaat[$i] = array($userRow['vakantienaam'], $userRow['begindatum'], $userRow['einddatum']);
-      $i++;
-    }
+    <div id="contentwrapper">
+      Laat boekingen zien: <br>
+      <?php
 
-    foreach($resultaat as $oefen){
-      print(" <a href=\"http://localhost/GFY1-03/admin/boekopvraagsript.php\">" . $resultaat[$teller][0] . ",  "
-      . $resultaat[$teller][1] . ",  " . $resultaat[$teller][2] . "</a>");
-      print("</br>");
-      $teller++;
-    }
-    ?>
+      $pdo = newPDO();
+      $stmt = $pdo->prepare("SELECT vakantienaam, begindatum, einddatum FROM boeking b JOIN reis r ON b.idKlant=r.idklant ORDER BY begindatum ");
+      $stmt->execute();
+      $teller = 0;
+      $i = 0;
+      $resultaat = array();
+      while($userRow = $stmt-> fetch()){
+        $resultaat[$i] = array($userRow['vakantienaam'], $userRow['begindatum'], $userRow['einddatum']);
+        $i++;
+      }
+      ?>
+      <div class="row">
+        <div class="col-md-6">
 
+          <table class="table table-striped table-hover nieuwsberichtenbewerken">
+            <th>Vakantienaam</th><th>Begindatum</th><th>Einddatum</th>
+
+            <?php
+            foreach($resultaat as $oefen){
+              print ("<a href='http://localhost/GFY1-03/admin/boekopvraagsript.php'><tr>");
+              print(" <td> " .
+              $resultaat[$teller][0] . "</td><td>"
+              . $resultaat[$teller][1] . "</td><td>" .
+              $resultaat[$teller][2] . "</a></td>");
+              print ("</tr></a>");
+
+              $teller++;
+            }
+            ?>
+
+          </table>
+        </div>
+      </div>
+    </div>
   </div>
 </body>
 </html>
