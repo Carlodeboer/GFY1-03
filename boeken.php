6fbaf57f0a7d758e38ca8d9ec426f2131a75d994
Carlodeboer <carlodb@hotmail.com>
2016-12-20 10:06:10 +0100

    boeken aangepast

--- boeken.php
+++ boeken.php
@@ -11,6 +11,10 @@
           <div id="content">
                <div id="contentwrapper">
                     <h2>Boeken</h2>
+
+                    <div class="row">
+                    <div class="col-md-4">
+
                     <form method="POST" action="boekengegevens.php" class="form-horizontal">
                          <div class="form-group label-static is-empty">
                               <label for="i5i" class="control-label">Begindatum*</label>
@@ -86,7 +90,149 @@
                          </div>
                     </form>
                </div>
-          </div>
+          <!-- </div> -->
+
+
+
+        <?php
+        $pdo = newPDO();
+        date_default_timezone_set("Europe/Amsterdam");
+        $date = strtotime(date("Y-m-d"));
+        $day = date('d', $date);
+
+        if (!isset($year)) {
+            $year = date('Y', $date);
+        }
+        if (!isset($month)) {
+            $month = date('m', $date);
+        }
+
+        if (!isset($_SESSION['jaarnummer'])) {
+            $_SESSION['jaarnummer'] = date('Y', $date);
+        }
+
+
+        if (!isset($_SESSION['maandnummer'])) {
+            $_SESSION['maandnummer'] = date('m', $date);
+        }
+
+
+        if (isset($_POST['volgende'])) {
+            ++$_SESSION['maandnummer'];
+            if ($_SESSION['maandnummer'] > 12) {
+                $_SESSION['maandnummer'] = 1;
+                $_SESSION['jaarnummer'] ++;
+            }
+        }
+
+        if (isset($_POST['vorige'])) {
+            --$_SESSION['maandnummer'];
+            if ($_SESSION['maandnummer'] < 1) {
+                $_SESSION['jaarnummer'] --;
+                $_SESSION['maandnummer'] = 12;
+            }
+        }
+
+        $month = $_SESSION['maandnummer'];
+        $year = $_SESSION['jaarnummer'];
+
+        $firstDay = mktime(0, 0, 0, $month, 1, $year);
+        $title = strftime('%B', $firstDay);
+        $dayOfWeek = date('D', $firstDay);
+        $daysInMonth = cal_days_in_month(0, $month, $year);
+        /* Get the name of the week days */
+        $timestamp = strtotime('next Sunday');
+        $weekDays = array();
+        for ($i = 0; $i < 7; $i++) {
+            $weekDays[] = strftime('%a', $timestamp);
+            $timestamp = strtotime('+1 day', $timestamp);
+        }
+        $blank = date('w', strtotime("{$year}-{$month}-01"));
+        ?>
+
+
+<div class="col-md-8">
+                <table id="calendar">
+
+                    <tr>
+                        <th colspan="7">
+                                    <form method="POST">
+                                        <input type="submit" name="vorige" value="Vorige" class="btn btn-raised btn-primary">
+                                        </div>
+
+                                        <div class="col-md-6"><?php print("{$title} {$year}"); ?>
+                                        </div>
+
+                                            <input type="submit" name="volgende" value="Volgende" class="btn btn-raised btn-primary">
+                                        </div>
+
+                                        </th>
+
+                                    </form>
+                                    </tr>
+                                    <tr>
+
+
+<?php foreach ($weekDays as $key => $weekDay) { ?>
+                                            <td class="text-center">
+                                            <?php echo $weekDay ?>
+                                            </td>
+                                            <?php } ?>
+                                    </tr><tr>
+                                        <?php
+                                        for ($i = 0; $i < $blank; $i++) {
+                                            print("<td></td>");
+                                        }
+
+                                        $stmt = $pdo->prepare("SELECT * FROM beschikbaarheid
+                                        WHERE Year(begindatum) = ?
+                                        AND Month(begindatum) = ?
+                                        ORDER BY einddatum");
+                                        $stmt->execute(array($_SESSION['jaarnummer'], $_SESSION['maandnummer']));
+
+                                        $objArray = array();
+                                        while ($userRow = $stmt->fetch()) {
+                                            $sDayStart = date('j', strtotime($userRow['begindatum']));
+                                            $sDayEnd = date('j', strtotime($userRow['einddatum']));
+
+                                            for ($dd = $sDayStart; $dd <= $sDayEnd; $dd++) {
+                                                $objArray[$dd] = $userRow['omschrijving'];
+                                            }
+                                        }
+
+                                        $y = 0;
+                                        for ($i = 1; $i <= $daysInMonth; $i++) {
+
+
+                                            echo "<td>";
+                                            if (isset($objArray[$i])) {
+                                                echo "<span style='color:lightgrey'>" . $i . ": Niet beschikbaar </span>" ;
+                                            } else {
+                                                echo $i;
+                                            }
+
+
+                                            echo "</td>";
+
+                                            if (($i + $blank) % 7 == 0) {
+                                                echo "</tr><tr>";
+                                            }
+
+                                            $y++;
+                                        }
+                                        for ($i = 0; ($i + $blank + $daysInMonth) % 7 != 0; $i++) {
+                                            print("<td></td>");
+                                        }
+                                        ?>
+                                    </tr>
+                                    </table>
+                                </div>
+                                </div>
+                                </div>
+
+
+
+
           <?php include 'footer.php'; ?>
      </div>
      <script>
