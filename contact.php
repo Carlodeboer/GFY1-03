f1fb022aec4a34ccac512468f1f0e877c17598b1
Floris de Grip <Floris@MacBook-Pro-van-Floris.local>
2016-12-21 14:29:08 +0100

    1337

--- contact.php
+++ contact.php
@@ -18,7 +18,7 @@
                     <h2><?php print $labels[0]; ?></h2>
 
                         <div class="col-md-6">
-                           
+
 
                             <form name="contactform" method="post" action="contactscript.php">
 
@@ -68,8 +68,8 @@
 
                                 </div>
 
-                                <div class="g-recaptcha" data-sitekey="6LdvmA0UAAAAANG5pBHPYoqw1-MTYUJRngrA88zK"></div><br><br><br><br><br><br><br>
-                                <input type="submit" value=<?php print "\"" . $labels[7] . "\""; ?> class="btn btn-raised btn-primary">
+                                <div class="g-recaptcha" data-sitekey="6LdvmA0UAAAAANG5pBHPYoqw1-MTYUJRngrA88zK"></div><br>
+                                <input type="submit" value=<?php print "\"" . $labels[7] . "\""; ?> class="btn btn-raised btn-primary contactformulierverzendenknop">
 
                             </form>
                         </div>
@@ -77,14 +77,11 @@
                         <div class="col-md-2">
                         </div>
 
-
-                        <div class="col-md-5">
                             <div id="googlemaps">
                                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2428.853044415557!2d6.079371215736348!3d52.49989967981054!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c7ded4f4f95b31%3A0xb1a2f2cf9bba075!2sCampus+1%2C+8017+Zwolle!5e0!3m2!1snl!2snl!4v1481550637316"
                                         width="450" height="375" frameborder="0" style="border:0" allowfullscreen></iframe>
                             </div>
-                        </div>
-                    </div>
+                     </div>
 
                 </div>
             </div>
