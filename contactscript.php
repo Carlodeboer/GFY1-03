93b07d021469fd1a6ba82503acdbf1e0c6ea04d9
Sophie Roos <sophie@famroos.nu>
2016-12-22 16:56:24 +0100

    Uitlijnen

--- contactscript.php
+++ contactscript.php
@@ -1,18 +1,18 @@
 <?php define("toegang", true); ?>
 <html>
-    <head>
-        <title>Motocross</title>
-        <?php include 'head.php';?>
-    </head>
-    <body>
-        <div id="container">
+<head>
+     <title>Motocross</title>
+     <?php include 'head.php';?>
+</head>
+<body>
+     <div id="container">
           <?php include 'header.php';?>
 
 
           <div id="content">
-                <?php
+               <?php
 
-                if(isset($_POST['email'])) {
+               if(isset($_POST['email'])) {
 
 
 
@@ -28,17 +28,17 @@
 
                     function died($error) {
 
-                        // your error code can go here
+                         // your error code can go here
 
-                        echo "<br> We are very sorry, but there were error(s) found with the form you submitted. ";
+                         echo "<br> We are very sorry, but there were error(s) found with the form you submitted. ";
 
-                        echo "These errors appear below.<br /><br />";
+                         echo "These errors appear below.<br /><br />";
 
-                        echo $error."<br /><br />";
+                         echo $error."<br /><br />";
 
-                        echo "Please go back and fix these errors.<br /><br />";
+                         echo "Please go back and fix these errors.<br /><br />";
 
-                        die();
+                         die();
 
                     }
 
@@ -48,17 +48,17 @@
 
                     if(!isset($_POST['first_name']) ||
 
-                        !isset($_POST['last_name']) ||
+                    !isset($_POST['last_name']) ||
 
-                        !isset($_POST['subject']) ||
+                    !isset($_POST['subject']) ||
 
-                        !isset($_POST['email']) ||
+                    !isset($_POST['email']) ||
 
-                        !isset($_POST['telephone']) ||
+                    !isset($_POST['telephone']) ||
 
-                        !isset($_POST['comments'])) {
+                    !isset($_POST['comments'])) {
 
-                        died('We are sorry, but there appears to be a problem with the form you submitted.');
+                         died('We are sorry, but there appears to be a problem with the form you submitted.');
 
                     }
 
@@ -81,43 +81,43 @@
 
                     $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
 
-                  if(!preg_match($email_exp,$email_from)) {
+                    if(!preg_match($email_exp,$email_from)) {
 
-                    $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
+                         $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
 
-                  }
+                    }
 
                     $string_exp = "/^[A-Za-z .'-]+$/";
 
-                  if(!preg_match($string_exp,$first_name)) {
+                    if(!preg_match($string_exp,$first_name)) {
 
-                    $error_message .= 'The First Name you entered does not appear to be valid.<br />';
+                         $error_message .= 'The First Name you entered does not appear to be valid.<br />';
 
-                  }
+                    }
 
-                  if(!preg_match($string_exp,$last_name)) {
+                    if(!preg_match($string_exp,$last_name)) {
 
-                    $error_message .= 'The Last Name you entered does not appear to be valid.<br />';
+                         $error_message .= 'The Last Name you entered does not appear to be valid.<br />';
 
-                  }
+                    }
 
-                  if(!preg_match($string_exp,$subject)) {
+                    if(!preg_match($string_exp,$subject)) {
 
-                    $error_message .= 'The Subject you entered does not appear to be valid.<br />';
+                         $error_message .= 'The Subject you entered does not appear to be valid.<br />';
 
-                  }
+                    }
 
-                  if(strlen($comments) < 2) {
+                    if(strlen($comments) < 2) {
 
-                    $error_message .= 'The Comments you entered do not appear to be valid.<br />';
+                         $error_message .= 'The Comments you entered do not appear to be valid.<br />';
 
-                  }
+                    }
 
-                  if(strlen($error_message) > 0) {
+                    if(strlen($error_message) > 0) {
 
-                    died($error_message);
+                         died($error_message);
 
-                  }
+                    }
 
                     $email_message = "Form details below.\n\n";
 
@@ -125,9 +125,9 @@
 
                     function clean_string($string) {
 
-                      $bad = array("content-type","bcc:","to:","cc:","href");
+                         $bad = array("content-type","bcc:","to:","cc:","href");
 
-                      return str_replace($bad,"",$string);
+                         return str_replace($bad,"",$string);
 
                     }
 
@@ -147,41 +147,41 @@
 
 
 
-                // create email headers
+                    // create email headers
 
-                $headers = 'From: '.$email_from."\r\n".
+                    $headers = 'From: '.$email_from."\r\n".
 
-                'Reply-To: '.$email_from."\r\n" .
+                    'Reply-To: '.$email_from."\r\n" .
 
-                'X-Mailer: PHP/' . phpversion();
+                    'X-Mailer: PHP/' . phpversion();
 
-                @mail($email_to, $email_subject, $email_message, $headers);
+                    @mail($email_to, $email_subject, $email_message, $headers);
 
-$pdo=newPDO();
-                //idbericht, voornaam, achternaam, email, telefoonnummer, onderwerp, bericht, datum
-                $stmt = $pdo->prepare("INSERT INTO contactformulier (voornaam, achternaam, email, telefoonnummer, onderwerp, bericht, datum) VALUES (?, ?, ?, ?, ?, ?, ?)");
-                $stmt->execute(array($first_name, $last_name, $email_from, $telephone, $subject, $comments, date("Y/m/d" . "  " . "H:i:sa")));
-/*
-                $stmt = $pdo->prepare("INSERT INTO users (username,email,password) VALUES (?,?,?)");
-                $stmt->execute(array($username, $email, $passwordhash));
-                */
-                $pdo = NULL;
-
-
-                ?>
-                <br>
-<?php
-                $labels = contactformulierTaal();
-                print($labels[8]);
- ?>
+                    $pdo=newPDO();
+                    //idbericht, voornaam, achternaam, email, telefoonnummer, onderwerp, bericht, datum
+                    $stmt = $pdo->prepare("INSERT INTO contactformulier (voornaam, achternaam, email, telefoonnummer, onderwerp, bericht, datum) VALUES (?, ?, ?, ?, ?, ?, ?)");
+                    $stmt->execute(array($first_name, $last_name, $email_from, $telephone, $subject, $comments, date("Y/m/d" . "  " . "H:i:sa")));
+                    /*
+                    $stmt = $pdo->prepare("INSERT INTO users (username,email,password) VALUES (?,?,?)");
+                    $stmt->execute(array($username, $email, $passwordhash));
+                    */
+                    $pdo = NULL;
 
 
-                <?php
-                }
+                    ?>
+                    <br>
+                    <?php
+                    $labels = contactformulierTaal();
+                    print($labels[8]);
+                    ?>
 
-                ?>
-              </div>
-            <?php include 'footer.php';?>
-        </div>
-    </body>
+
+                    <?php
+               }
+
+               ?>
+          </div>
+          <?php include 'footer.php';?>
+     </div>
+</body>
 </html>
