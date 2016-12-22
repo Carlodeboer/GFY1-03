3549049801bfcdd82712552da136431db1fef887
moonbubble <r.deroo@solcon.nl>
2016-12-22 21:56:17 +0100

    delete stukje
    stukje uit boekopvraag verwijderd wat eigenlijk ook in boekopvraagscript
    staat

--- admin/boekopvraag.php
+++ admin/boekopvraag.php
@@ -32,15 +32,7 @@
                                    </tr>";
                               }
 
-                              if (isset($_GET['boekingID'])) {
-                                   $boekingID = $_GET['boekingID'];
-
-                                   $stmt = $pdo->prepare("SELECT idKlant, begindatum, einddatum, aantalPersonen, vervoerHeen, vervoerTerug, locatie, opmerking, status, betaling
-                                        FROM boeking
-                                        WHERE idKlant=?");
-                                        $stmt->execute(array($boekingID));
-                                        $boekingGegevens = $stmt->fetch();
-                                   }
+                              
                                    ?>
 
                               </table>
