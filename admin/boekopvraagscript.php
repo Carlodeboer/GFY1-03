01770c7b2141ca98f7ecb0b9974f566609eccff1
moonbubble <r.deroo@solcon.nl>
2016-12-22 15:00:13 +0100

    BOEKOPVRAAGSCRUPT BIJGEWERKT

--- admin/boekopvraagscript.php
+++ admin/boekopvraagscript.php
@@ -6,7 +6,21 @@
   <div id="container">
     <div id="contentwrapper">
       <?php
-      print("this is nice");
+      print("this is nice <br><br>");
+
+        $pdo = newPDO();
+      if (isset($_GET['boekingID'])) {
+          $boekingID = $_GET['boekingID'];
+
+          $stmt = $pdo->prepare("SELECT idKlant, begindatum, einddatum, aantalPersonen, vervoerHeen, vervoerTerug, locatie, opmerking, status, betaling
+                  FROM boeking
+                  WHERE idKlant=?");
+          $stmt->execute(array($boekingID));
+          $boekingGegevens = $stmt->fetch();
+      }
+
+      print($boekingGegevens["idKlant"]);
+
       ?>
     </div>
   </div>
