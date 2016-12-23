5b717f2a6a30a8410b1de0d3ba3b3d7249d525f5
Sophie Roos <sophie@famroos.nu>
2016-12-23 10:05:30 +0100

    Emailadres + uitlijnen

--- admin/berichtopvraagsubmit.php
+++ admin/berichtopvraagsubmit.php
@@ -1,25 +1,25 @@
 <?php
-    include "../toegang.php";
+include "../toegang.php";
 ?>
 <br><br>
 <div id="contentwrapper">
-<form name="contactform" method="post" action="beheerpaneel.php?beheer=berichtopvraag">
-<table width="450px">
+     <form name="contactform" method="post" action="beheerpaneel.php?beheer=berichtopvraag">
+          <table width="450px">
 
-<tr>
- <td valign="top">
-  <label for="email">Email Adres *</label>
- </td>
- <td valign="top">
-  <input  type="text" name="email" maxlength="80" size="30">
- </td>
-</tr>
+               <tr>
+                    <td valign="top">
+                         <label for="email">E-mailadres *</label>
+                    </td>
+                    <td valign="top">
+                         <input  type="text" name="email" maxlength="80" size="30">
+                    </td>
+               </tr>
 
-<tr>
- <td colspan="2" style="text-align:center">
-  <br><input type="submit" name="zoekBerichten" value="Submit" class="btn-main">
- </td>
-</tr>
-</table>
-</form>
+               <tr>
+                    <td colspan="2" style="text-align:center">
+                         <br><input type="submit" name="zoekBerichten" value="Submit" class="btn-main">
+                    </td>
+               </tr>
+          </table>
+     </form>
 </div>
