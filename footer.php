c4dae2ec193e6361c9dd9b932e0c3cf2e9af309a
Floris de Grip <Floris@mbp-van-floris.home>
2017-01-04 15:35:54 +0100

    nee

--- footer.php
+++ footer.php
@@ -26,7 +26,7 @@
     </div>
     <div class="col-md-4" style="float:right">
 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2443.549416940136!2d5.956437915725985!3d52.233403579760925!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c7c7a21bf0f591%3A0x7ec280ce253a9027!2sKoninginnelaan+239%2C+7315+DW+Apeldoorn!5e0!3m2!1snl!2snl!4v1483369231647"
-        width="350" height="250" frameborder="0" style="border:0" allowfullscreen></iframe>    </div>
+        width="350" height="225" frameborder="0" style="border:0" allowfullscreen></iframe>    </div>
   </div>
 </div>
 
