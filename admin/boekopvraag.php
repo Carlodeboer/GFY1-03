cf77bd80d2512210cece383beb4d227402a09487
moonbubble <r.deroo@solcon.nl>
2017-01-06 14:00:21 +0100

    sorry nog meer onzin

--- admin/boekopvraag.php
+++ admin/boekopvraag.php
@@ -84,8 +84,12 @@
                                                   <form method='POST'>
                                                        <input type="hidden" name="idklant" value="<?php print($klantID)?>">
 
+
                                                        <input type="hidden" name="idReservering" value="<?php print($idReservering)?>">
                                                        <input type='submit' name='annuleren' value='Annuleren' class='btn btn-raised btn-warning' onclick="return confirm('Weet je het zeker?')">
+
+                                                       <input type='submit' name='annuleren' value='Annuleren' class='btn btn-raised btn-warning' onclick='return confirm('Weet je het zeker?')'>
+
 
                                                        <input type='submit' name='annuleren' value='Annuleren' class='btn btn-raised btn-warning' onclick='return confirm('Weet je het zeker?')'>
 
