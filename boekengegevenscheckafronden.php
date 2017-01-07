e54e65742308fc0b55762db5241f819725b652e8
Sophie Roos <sophie@famroos.nu>
2017-01-07 14:21:09 +0100

    Commentaar

--- boekengegevenscheckafronden.php
+++ boekengegevenscheckafronden.php
@@ -12,109 +12,69 @@
                     <?php
                     $labels = boekenTaal4();
                     $j = 0;
-                         if (isset($_POST["afronden"])) {
-                              extract($_SESSION["klantGegevens"]);
+                    if (isset($_POST["afronden"])) {
+                         // Haalt alle elementen uit array en maakt variablen met als naam de indes en waarde de key.
+                         extract($_SESSION["klantGegevens"]);
 
-                              date_default_timezone_set("Europe/Amsterdam");
-                              $datum = strtotime($begindatum);
-                              $weekjaar = date('Wy', $datum);
+                         date_default_timezone_set("Europe/Amsterdam");
 
-                              try {
-                                   $pdo = newPDO();
-                                   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                         // Weekjaar is het nummer van de week en het nummer van het jaar aan elkaar; wordt gebruikt om in te loggen.
+                         $datum = strtotime($begindatum);
+                         $weekjaar = date('Wy', $datum);
 
-                                   $pdo->beginTransaction();
+                         try {
+                              // Nieuwe verbinding database
+                              $pdo = newPDO();
+                              $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-                                   $stmt1 = $pdo->prepare("INSERT INTO reserveringen (begindatum, type, aantal) VALUES (?, 'boeking', ?)");
-                                   $stmt1->execute(array($begindatum, $aantalPersonen));
+                              $pdo->beginTransaction();
 
-                                   $stmt2 = $pdo->prepare("SELECT max(idReservering) FROM reserveringen");
-                                   $stmt2->execute(array());
-                                   $row2 = $stmt2->fetch();
-                                   $idReservering = $row2["max(idReservering)"];
+                              // Toevoegen aan database
+                              $stmt1 = $pdo->prepare("INSERT INTO reserveringen (begindatum, type, aantal) VALUES (?, 'boeking', ?)");
+                              $stmt1->execute(array($begindatum, $aantalPersonen));
 
-                                   $stmt3 = $pdo->prepare("INSERT INTO boeking (idReservering, begindatum, einddatum, aantalPersonen, vervoerHeen, vervoerTerug, opmerking) VALUES (?, ?, ?, ?, ?, ?, ?)");
-                                   $stmt3->execute(array($idReservering, $begindatum, $einddatum, $aantalPersonen, $vervoerHeen, $vervoerTerug, $opmerkingen));
+                              // Ophalen van het maximale id van de reservering zodat hij daarna toegevoegd kan worden bij het volgende statement.
+                              $stmt2 = $pdo->prepare("SELECT max(idReservering) FROM reserveringen");
+                              $stmt2->execute(array());
+                              $row2 = $stmt2->fetch();
+                              $idReservering = $row2["max(idReservering)"];
 
-                                   $stmt4 = $pdo->prepare("SELECT max(idKlant) FROM boeking");
-                                   $stmt4->execute(array());
-                                   $row4 = $stmt4->fetch();
-                                   $idKlant = $row4["max(idKlant)"];
+                              $stmt3 = $pdo->prepare("INSERT INTO boeking (idReservering, begindatum, einddatum, aantalPersonen, vervoerHeen, vervoerTerug, opmerking) VALUES (?, ?, ?, ?, ?, ?, ?)");
+                              $stmt3->execute(array($idReservering, $begindatum, $einddatum, $aantalPersonen, $vervoerHeen, $vervoerTerug, $opmerkingen));
 
-                                   for ($i = 1; $i <= $_SESSION["klantGegevens"]["aantalPersonen"]; $i++) {
-                                        $adres = (${"straat" . $i} . " " . ${"huisnummer" . $i});
-                                        $stmt5 = $pdo->prepare("INSERT INTO klantgegevens (idklant, persoon, voornaam, achternaam, adres, postcode, woonplaats, land, gebdatum, telefoonnummer, email, kledingmaat, schoenmaat, bijzonderheden) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
-                                        $stmt5->execute(array($idKlant, $i, ${"voornaam" . $i}, ${"achternaam" . $i}, $adres, ${"postcode" . $i}, ${"woonplaats" . $i}, ${"land" . $i}, ${"geboortedatum" . $i}, ${"telefoonnummer" . $i},
-                                        ${"email" . $i}, ${"kledingmaat" . $i}, ${"schoenmaat" . $i}, ${"bijzonderheden" . $i}));
-                                   }
+                              // Ophalen van het maximale id van de klant zodat hij daarna toegevoegd kan worden bij het volgende statement.
+                              $stmt4 = $pdo->prepare("SELECT max(idKlant) FROM boeking");
+                              $stmt4->execute(array());
+                              $row4 = $stmt4->fetch();
+                              $idKlant = $row4["max(idKlant)"];
 
-                                   $stmt6 = $pdo->prepare("INSERT INTO gebruikers (idGebruiker, gebruikersnaam, wachtwoord, privilegeniveau) VALUES (?, ?, ?, 1)");
-                                   $stmt6->execute(array($idKlant, $vakantienaam, $weekjaar));
-
-                                   $pdo->commit();
-                                   $pdo = NULL;
-
-                              ?><?php print($labels[$j]); $j++; ?><a href="login.php"><?php print($labels[$j]); $j++; ?></a>
-                                   <?php print($labels[$j]); $j++;
-                                   print("<p class='text-primary'>" . $vakantienaam . "</p>");?><?php print($labels[$j]); $j++; ?><?php
-                                   print("<p class='text-primary'>" . $weekjaar . "</p>.");
-
-                                   for($i = 1; $i <= $aantalPersonen; $i++) {
-                                        $email_to = ${"email" . $i};
-                                        $email_to = "sophie@famroos.nu";
-                                        $email_from = "info@offroadcompassportugal.nl";
-                                        $email_subject = "Bericht verstuurd via contactformulier";
-                                        $email_message = "Beste " . ${"voornaam" . $i} . " " . ${"achternaam" . $i} . "\n\n";
-
-                                        $email_message .= "Bedankt voor het boeken van reis met Offroad Compass Portugal. U kunt uw reisinformatie inzien met de volgende gegevens:\n\n";
-
-                                        $email_message .= "Vakantienaam:" . $vakantienaam . "\n";
-                                        $email_message .= "Vakantieweek:" . $weekjaar . "\n";
-
-                                        $headers = 'From: '.$email_from."\r\n".
-
-                                        'Reply-To: '.$email_from."\r\n" .
-
-                                        'X-Mailer: PHP/' . phpversion();
-
-                                        //versturen van de mail
-                                        @mail($email_to, $email_subject, $email_message, $headers);
-                                   }
-                                   //
-                                   //      // $email_message .= "Begindatum:" . $begindatum . "\n";
-                                   //      // $email_message .= "Einddatum:" . $einddatum . "\n";
-                                   //      // $email_message .= "Vervoer van luchthaven Lissabon:" . $vervoerHeen . "\n";
-                                   //      // $email_message .= "Vervoer naar luchthaven Lissabon:" . $vervoerTerug . "\n";
-                                   //      // $email_message .= "Aantal personen:" . $aantalPersonen . "\n";
-                                   //      // if ($opmerkingen != NULL) {
-                                   //      //      $email_message .= "Opmerkingen:" . $opmerkingen . "\n";
-                                   //      // }
-                                   //      //
-                                   //      // $email_message .= "Naam:" . ${"voornaam" . $i} . " " . ${"voornaam" . $i} . "\n";
-                                   //      // $email_message .= "Adres:" . ${"straat" . $i} . " " . ${"huisnummer" . $i} . "\n";
-                                   //      // $email_message .= "Postcode:" . ${"postcode" . $i} . "\n";
-                                   //      // $email_message .= "Woonplaats:" . ${"woonplaats" . $i} . "\n";
-                                   //      // $email_message .= "Land:" . ${"land" . $i} . "\n";
-                                   //      // $email_message .= "Telefoonnummer:" . ${"telefoonnummer" . $i} . "\n";
-                                   //      // $email_message .= "Email:" . ${"email" . $i} . "\n";
-                                   //      // $email_message .= "Kledingmaat:" . ${"kledingmaat" . $i} . "\n";
-                                   //      // $email_message .= "Schoenmaat:" . ${"schoenmaat" . $i} . "\n";
-                                   //      // if (${"Bijzonderheden" . $i} != NULL) {
-                                   //      //      $email_message .= "Bijzonderheden:" . ${"Bijzonderheden" . $i} . "\n";
-                                   //      // }
-                                   //      //
-                                   //      // $email_message .= "\n\nVoor eventuele vragen kunt u contact opnemen via het contactformulier\n\n";
-                                   //      // $email_message .= "Met vriendelijke groet,\nMichael Mairhofer\nOffroad Compass Portugal";
-                                   //
-                                   //
-                                   // }
-
-                              } catch (Exception $e) {
-                                   print("Uw boeking is niet succesvol verwerkt. Neem contact op met de beheerder.");
+                              // Voor elke persoon die mee gaat per boeking worden apart de klantgegevens opgeslagen.
+                              for ($i = 1; $i <= $_SESSION["klantGegevens"]["aantalPersonen"]; $i++) {
+                                   $adres = (${"straat" . $i} . " " . ${"huisnummer" . $i});
+                                   $stmt5 = $pdo->prepare("INSERT INTO klantgegevens (idklant, persoon, voornaam, achternaam, adres, postcode, woonplaats, land, gebdatum, telefoonnummer, email, kledingmaat, schoenmaat, bijzonderheden) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+                                   $stmt5->execute(array($idKlant, $i, ${"voornaam" . $i}, ${"achternaam" . $i}, $adres, ${"postcode" . $i}, ${"woonplaats" . $i}, ${"land" . $i}, ${"geboortedatum" . $i}, ${"telefoonnummer" . $i},
+                                   ${"email" . $i}, ${"kledingmaat" . $i}, ${"schoenmaat" . $i}, ${"bijzonderheden" . $i}));
                               }
+
+                              // Account aanmaken zodat er later kan worden ingelogd.
+                              $stmt6 = $pdo->prepare("INSERT INTO gebruikers (idGebruiker, gebruikersnaam, wachtwoord, privilegeniveau) VALUES (?, ?, ?, 1)");
+                              $stmt6->execute(array($idKlant, $vakantienaam, $weekjaar));
+
+                              $pdo->commit();
+                              $pdo = NULL;
+
+                              // Feedback geven aan de gebruiker met linkje naar inlogscherm.
+                              print($labels[$j]); $j++; ?><a href="login.php"><?php print($labels[$j]); $j++; ?></a>
+                              <?php print($labels[$j]); $j++;
+                              print("<strong class='text-primary'>" . $vakantienaam . "</strong>");?><?php print($labels[$j]); $j++; ?><?php
+                              print("<strong class='text-primary'>" . $weekjaar . "</strong>.");
+
+                         } catch (Exception $e) {
+                              // Als de boeking niet succesvol verwerkt is geeft hij een melding.
+                              print("Uw boeking is niet succesvol verwerkt. Neem contact op met de beheerder.");
                          }
-                         ?>
-                    </p>
+                    }
+                    ?>
                </div>
           </div>
           <?php include "footer.php";?>
