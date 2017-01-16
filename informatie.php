fd183df7018b12e92023c75c6eccdb1a8a930f31
moonbubble <r.deroo@solcon.nl>
2017-01-16 17:33:35 +0100

    adres in kaart accommodatie veranderd

--- informatie.php
+++ informatie.php
@@ -30,7 +30,7 @@
                          ?>
                     </div>
                     <div id="kaart">
-                         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12486.016844422375!2d-9.106586769673552!3d38.445485174283974!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd19518b4a6daf8d%3A0xd16dc61d905e068b!2sSesimbra%2C+Portugal!5e0!3m2!1snl!2snl!4v1483805616779"
+                         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3123.5436943162167!2d-9.111479085020921!3d38.47509457963791!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd194dfb2226e089%3A0x86637cad7c24d253!2sR.+Camilo+Castelo+Branco+107%2C+Portugal!5e0!3m2!1snl!2snl!4v1484584009613"
                          width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
                     </div>
                </div>
